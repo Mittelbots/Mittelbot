f0fdb5b8f8f621fee7d77451c8056fe76d91313f
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-12 01:46:03 +0100

    Create ping command

---
+++ src/commands/ping/ping.js
@@ -0,0 +1,18 @@
+const config = require('../../../config.json');
+
+module.exports.run = async (bot, message, args) => {
+    if(config.deleteCommandsAfterUsage == 'true') {
+        message.delete();
+    }
+    message.channel.send(`Pong!`).then(msg => {
+        setTimeout(() => {
+            msg.edit(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`)
+            return;
+        }, 1000);
+    })
+
+}
+
+module.exports.help = {
+    name:"ping"
+}
