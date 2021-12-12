df083941ded3052badcc15d98a6c705c809da145
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-12 14:52:39 +0100

    added ban command

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,4 +1,4 @@
-const {Permissions} = require('discord.js');
+const {Permissions, MessageEmbed} = require('discord.js');
 const config = require('../../../config.json');
 
 module.exports.run = async (bot, message, args) => {
@@ -20,9 +20,74 @@ module.exports.run = async (bot, message, args) => {
             setTimeout(() => msg.delete(), 5000);
         });
     }
-    message.channel.send('Under construction');
+   
+    let Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if (!Member) return message.reply(`<@${message.author.id}> You have to mention a user`);
+    if (Member.id === message.author.id) return message.reply(`You can't ban yourself.`);
+
+    if(Member.user.bot) message.reply(`Do you really want to ban <@${Member.user.id}>? It's a Bot.`).then(() => {      
+        let msg_filter = m => m.author.id === message.author.id;
+        message.channel.awaitMessages({filter: msg_filter, max: 1})
+        .then(collected => {
+            collected = collected.first();
+            if(collected.content.toUpperCase() == 'YES' || collected.content.toUpperCase() == 'Y') {
+                if(!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.reply(`You don't have the permission to ban a bot.`)
+                try {
+                    Member.ban();
+                    return message.reply(`<@${Member.id}>${config.successmessages.banned}`);
+                }catch(err) {
+                    console.log(err);
+                    return message.reply(`${config.errormessages.botnopermission}`);
+                }
+
+            }else if (collected.content.toUpperCase() == 'NO' || collected.content.toUpperCase() == 'N') {
+                return message.channel.send(`Terminated`).then(msg => {
+                    setTimeout(() => msg.delete(), 5000);
+                });
+            }else {
+                return message.channel.send(`Terminated: Invalid Response`).then(msg => {
+                    setTimeout(() => msg.delete(), 5000);
+                });
+            }
+        });
+    });  
+    // If Member is not a bot //
+
+    let isMod = false;
+    for(let i in config.modroles) {
+        if(Member.roles.cache.find(r => r.name === config.modroles[i]) !== undefined) {
+            isMod = true;
+            break;
+        }
+    }
+
+    if(isMod) return message.channel.send(`<@${message.author.id}> You can't ban a Moderator!`)
+
+    let reason = args.slice(1).join(" ");
+    if(!reason) return message.channel.send('Please add a reason!');
+
+
+    var Embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(`**Member Banned!**`)
+    .addField(`Moderator`, `<@${message.author.id}> (${message.author.id})`)
+    .addField(`Banned Member`, `<@${Member.user.id}> (${Member.user.id})`)
+    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+    .setTimestamp();
+
+    try {
+        Member.ban();
+        message.reply(`<@${Member.id}>${config.successmessages.banned} `);
+        return message.channel.send({embeds: [Embed]})
+    }catch(err) {
+        console.log(err);
+        return message.reply(`${config.errormessages.botnopermission}`);
+    }
+
 }
 
 module.exports.help = {
-    name:"ban"
+    name:"ban",
+    description: "Ban an User",
+    usage: "ban <Mention User> <Reason>"
 }
