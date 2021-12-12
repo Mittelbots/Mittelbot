6c2c4ab107ed893bbf6fa024b1fc6106a4c888ce
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-12 01:45:38 +0100

    Create mute command

---
+++ src/commands/moderation/mute.js
@@ -0,0 +1,71 @@
+const {
+    MessageEmbed,
+    Permissions
+} = require('discord.js');
+const config = require('../../../config.json');
+
+module.exports.run = async (bot, message, args) => {
+    if (config.deleteModCommandsAfterUsage == 'true') {
+        message.delete();
+    }
+
+    if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) return message.channel.send(`<@${message.author.id}> You dont have permission for this Command!`);
+
+    let Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if (!Member) return message.channel.send(`<@${message.author.id}> You have to mention a user`);
+
+    if(Member.user.bot) return message.channel.send(`You can't mute <@${Member.user.id}>. It's a Bot.`)
+
+    var MutedRole;
+
+    try {
+        MutedRole = message.guild.roles.cache.find(role => role.name === "Muted").id;
+    }catch(err) {
+        message.channel.send(`No Mutedrole detected! I'll create one for you.`);
+        MutedRole = await message.guild.roles.create({
+            name: 'Muted',
+            color: 'BLUE',
+            reason: 'Automatically created "Muted" Role.'
+        });
+        message.channel.send(`Role created!`);   
+    }
+
+    let reason = args.slice(1).join(" ");
+    if(!reason) return message.channel.send('Please add a reason!');
+
+    let time = args.slice(2).join(" ");
+    function isNum(val){
+        return !isNaN(val)
+      }
+    if(!isNum(time)) return message.channel.send('Time have to be a number!'); else message.channel.send('Tempmute is under construction')
+
+    if (Member.roles.cache.has(MutedRole)) {
+        return message.channel.send(`Member Is Already Muted!`);
+    }
+
+
+
+    var Embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(`**Member Muted!**`)
+    .addField(`Moderator`, `<@${message.author.id}> (${message.author.id})`)
+    .addField(`Muted Member`, `<@${Member.user.id}> (${Member.user.id})`)
+    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+    .addField(`Time`, `${time || "No Time Provided!"}`)
+    .setTimestamp();
+
+    try {
+        Member.roles.add([MutedRole]);
+        return message.channel.send({embeds: [Embed]});
+    }
+    catch(err) {
+        console.log(err);
+        message.channel.send(config.errormessages.general)
+    }
+}
+
+module.exports.help = {
+    name: "mute",
+    description: "Mute a User",
+    usage: "Mute <Mention User> | <Reason>"
+}
