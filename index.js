a96d05e9f2883fda50c7b4de44d84ed0348151c2
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-12 01:55:12 +0100

    Added Discord Webhook

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const fs = require('fs');
 const config = require('./config.json');
 const defaultCooldown = new Set();
 
-
 const bot = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]
 });
 bot.commands = new Discord.Collection();
-
 
 let modules = fs.readdirSync('./src/commands/');
 modules.forEach((module) => {
