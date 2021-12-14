5411304720856bb3c746efbf3f90cdb3c507c020
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-14 22:00:53 +0100

    added time to ban  & cleaned some code

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,5 +1,8 @@
 const {Permissions, MessageEmbed} = require('discord.js');
 const config = require('../../../config.json');
+const { getFutureDate } = require('../../../utils/functions/getFutureDate');
+const { getModTime } = require('../../../utils/functions/getModTime');
+const { database } = require('../../db/db');
 
 module.exports.run = async (bot, message, args) => {
     if(config.deleteModCommandsAfterUsage  == 'true') {
@@ -9,7 +12,6 @@ module.exports.run = async (bot, message, args) => {
     var hasPermission = false
     for(let i in config.modroles) {
         if(i == "trialmoderator") continue;
-
         if(message.member.roles.cache.find(r => r.name === config.modroles[i]) !== undefined) {
             hasPermission = true;
             break;
@@ -28,6 +30,8 @@ module.exports.run = async (bot, message, args) => {
 
     let reason = args.slice(1).join(" ");
     if(!reason) return message.channel.send('Please add a reason!');
+
+
 
 
     if(Member.user.bot) message.reply(`Do you really want to ban <@${Member.user.id}>? It's a Bot.`).then(() => {      
@@ -58,34 +62,64 @@ module.exports.run = async (bot, message, args) => {
     });  
     // If Member is not a bot //
 
-    let isMod = false;
     for(let i in config.modroles) {
         if(Member.roles.cache.find(r => r.name === config.modroles[i]) !== undefined) {
-            isMod = true;
-            break;
+            return message.channel.send(`<@${message.author.id}> You can't ban a Moderator!`)
         }
     }
 
-    if(isMod) return message.channel.send(`<@${message.author.id}> You can't ban a Moderator!`)
+    let time = args.slice(2).join(" ");
+    var dbtime = getModTime(time);
+    if(!dbtime) return message.reply(`Invalid Time [m, h, d]`);
+
+    var futuredate = getFutureDate(dbtime);
 
 
-    var Embed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`**Member Banned!**`)
-    .addField(`Moderator`, `<@${message.author.id}> (${message.author.id})`)
-    .addField(`Banned Member`, `<@${Member.user.id}> (${Member.user.id})`)
-    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
-    .setTimestamp();
+    database.query(`SELECT * FROM open_infractions WHERE user_id = ? AND ban = 1`, [Member.id], async (err, result) => {
+        if (err) {
+            console.log(err);
+            return message.reply(`${config.errormessages.databasequeryerror}`);
+        }
+        if (result.length > 0) {
+            for (let i in result) {
+                let currentdate = new Date().toLocaleString('de-DE', {timeZone: 'Europe/Berlin'})
+                currentdate = currentdate.replace(',', '').replace(':', '').replace(' ', '').replace(':', '').replace('.', '').replace('.', '').replace('.', '');
+                result[i].till_date = result[i].till_date.replace(',', '').replace(':', '').replace(' ', '').replace(':', '').replace('.', '').replace('.', '').replace('.', '');
 
-    try {
-        await Member.send({embeds: [Embed]});
-        await Member.ban({reason: reason});
-        await message.reply(`<@${Member.id}>${config.successmessages.banned} `);
-        return message.channel.send({embeds: [Embed]});
-    }catch(err) {
-        console.log(err);
-        return message.reply(`${config.errormessages.botnopermission}`);
-    }
+                if ((currentdate - result[i].till_date) <= 0) {
+                    return message.reply(`Member Is Already banned!`);
+                }
+            }
+        }
+
+        var Embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`**Member Banned!**`)
+            .addField(`Moderator`, `<@${message.author.id}> (${message.author.id})`)
+            .addField(`Member`, `<@${Member.user.id}> (${Member.user.id})`)
+            .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+            .addField(`Time`, `**${time}** `)
+            .setTimestamp();
+
+        try {
+            database.query(`INSERT INTO open_infractions (user_id, mod_id, ban, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?)`, [Member.id, message.author.id, 1, futuredate, reason, Math.random().toString(16).substr(2, 20)], async (err) => {
+                if(err) {
+                    console.log(err);
+                    return message.reply(`${config.errormessages.databasequeryerror}`);
+                }
+                await Member.send({embeds: [Embed]});
+                await message.reply(`<@${Member.id}>${config.successmessages.banned} `);
+                await Member.ban({reason: reason});
+                return message.channel.send({
+                    embeds: [Embed]
+                });
+            });
+        } catch (err) {
+            console.log(err);
+            message.channel.send(config.errormessages.general)
+        }
+
+    });
 
 }
 
