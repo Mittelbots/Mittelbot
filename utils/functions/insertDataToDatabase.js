7c2ee4783459480060fc261f7146b8f20b38ba3d
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-14 23:02:42 +0100

    added openinfraction database

--- utils/functions/insertDataToDatabase.js
+++ utils/functions/insertDataToDatabase.js
@@ -1,10 +1,14 @@
 const { database } = require("../../src/db/db");
 
 function insertDataToClosedInfraction (uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id) {
-    console.log(uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id)
     database.query('INSERT INTO closed_infractions (user_id, mod_id, mute, ban, warn, kick, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id], (err) => console.log(err));
+}
+
+function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id) {
+    database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, till_date, reason, infraction_id], (err) => console.log(err));
 }
 
 module.exports = {
     insertDataToClosedInfraction,
+    insertDataToOpenInfraction
 }
