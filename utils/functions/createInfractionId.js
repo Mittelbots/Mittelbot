7357cbd6ab773a84d08d0c2185ee62b51b82f7d2
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-15 19:19:10 +0100

    better code formatting

--- utils/functions/createInfractionId.js
+++ utils/functions/createInfractionId.js
@@ -8,16 +8,12 @@ function createInfractionId() {
     let infractionid = generate();
     database.query('SELECT infraction_id FROM open_infractions WHERE infraction_id = ?', [infractionid], (err, res) => {
         if(err) console.log(err)
-        if(res !== '') {
-            createInfractionId();
-        }
+        if(res !== '') createInfractionId();
     })
 
     database.query('SELECT infraction_id FROM closed_infractions WHERE infraction_id = ?', [infractionid], (err, res) => {
         if(err) console.log(err)
-        if(res !== '') {
-            createInfractionId();
-        }
+        if(res !== '') createInfractionId();
     });
     return infractionid;
 }
