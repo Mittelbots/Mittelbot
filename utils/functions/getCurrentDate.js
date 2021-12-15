cfaf9ca6c12634be90f6d2185be8c36ac6eebd62
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-15 21:25:10 +0100

    Create getCurrentDate.js

---
+++ utils/functions/getCurrentDate.js
@@ -0,0 +1,11 @@
+function getCurrentDate() {
+    return new Date().toLocaleString('de-DE', {timeZone: 'Europe/Berlin',
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'})
+}
+
+module.exports = {getCurrentDate}
