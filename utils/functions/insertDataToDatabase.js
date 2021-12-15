c8fed6aaff4913f3d89ea37932f2ec0865302014
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-15 20:15:17 +0100

    remove warn from function & added temprole database/function

--- utils/functions/insertDataToDatabase.js
+++ utils/functions/insertDataToDatabase.js
@@ -4,11 +4,16 @@ function insertDataToClosedInfraction (uid, modid, mute, ban, warn, kick, till_d
     database.query('INSERT INTO closed_infractions (user_id, mod_id, mute, ban, warn, kick, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id], (err) => console.log(err));
 }
 
-function insertDataToOpenInfraction (uid, modid, mute, ban, warn, till_date, reason, infraction_id) {
-    database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, warn, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, warn, till_date, reason, infraction_id], (err) => console.log(err));
+function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id) {
+    database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, till_date, reason, infraction_id], (err) => console.log(err));
+}
+
+function inserDataToTemproles (uid, role_id, till_date) {
+    database.query('INSERT INTO temproles (user_id, role_id, till_date) VALUES (?, ?, ?)', [uid, role_id, till_date])
 }
 
 module.exports = {
     insertDataToClosedInfraction,
-    insertDataToOpenInfraction
+    insertDataToOpenInfraction,
+    inserDataToTemproles
 }
