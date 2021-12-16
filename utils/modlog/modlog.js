cd30f8ff31022f6c2742d8b50a269c5b161de08f
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-16 22:45:05 +0100

    added modlog script

---
+++ utils/modlog/modlog.js
@@ -0,0 +1,30 @@
+const {MessageEmbed} = require('discord.js');
+const config = require('../../config.json');
+
+async function setNewModLogMessage(bot, type, moderator, member, reason, time) {
+    var modLogMessage = new MessageEmbed()
+    .setTitle(`**Member ${type}!**`)
+    .addField(`Moderator`, `<@${moderator}> (${moderator})`)
+    .addField(`${type} Member`, `<@${member}> (${member})`)
+    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+    .setTimestamp();
+
+    if(time) {
+        modLogMessage.addField(`Time`, `**${time}** `)
+    }
+
+    sendToModLog(bot, modLogMessage);
+    return;
+}
+
+function sendToModLog(bot, message) {
+    try {
+        bot.channels.cache.get(config.defaultChannels.modlog).send({embeds: [message]});
+        console.log('1')
+    }catch(err) {
+        console.log()
+    }
+}
+
+
+module.exports = {setNewModLogMessage}
