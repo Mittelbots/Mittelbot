3eda53e9b8205923ada77f62c7890ea616262d6d
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-17 02:06:02 +0100

    changed hasPerms to roleid instead rolename

--- utils/functions/hasPermissions.js
+++ utils/functions/hasPermissions.js
@@ -2,10 +2,10 @@ function hasPermission(message, adminOnly, modOnly) {
     const config = require('../../config.json');
     let hasPermission = false
     for (let i in config.modroles) {
-        if(adminOnly && message.member.roles.cache.find(r => r.name === config.modroles.trialmoderator || r.name === config.modroles.moderator)) continue;
-        if(modOnly && message.member.roles.cache.find(r => r.name === config.modroles.trialmoderator)) {continue};
+        if(adminOnly && message.member.roles.cache.find(r => r.id === config.modroles.trialmoderator || r.id === config.modroles.moderator)) continue;
+        if(modOnly && message.member.roles.cache.find(r => r.id === config.modroles.trialmoderator)) {continue};
         
-        if (message.member.roles.cache.find(r => r.name === config.modroles[i]) !== undefined) {
+        if (message.member.roles.cache.find(r => r.id === config.modroles[i]) !== undefined) {
             hasPermission = true;
             break;
         }
