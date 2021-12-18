e61544aae06ba571157a52a644f348b6cb690a32
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-18 15:43:05 +0100

    column will be moved to closed infraction database now

--- src/commands/moderation/unmute.js
+++ src/commands/moderation/unmute.js
@@ -3,6 +3,16 @@ const { setNewModLogMessage } = require('../../../utils/modlog/modlog');
 const { privateModResponse } = require('../../../utils/privatResponses/privateModResponses');
 const { publicModResponses } = require('../../../utils/publicResponses/publicModResponses');
 const { hasPermission } = require('../../../utils/functions/hasPermissions');
+const { database } = require('../../db/db');
+const { insertDataToClosedInfraction } = require('../../../utils/functions/insertDataToDatabase');
+
+
+async function deleteEntries(infraction) {
+    try {
+        insertDataToClosedInfraction(infraction[0].user_id, infraction[0].mod_id, infraction[0].mute, infraction[0].ban, 0, 0, infraction[0].till_date, infraction[0].reason, infraction[0].infraction_id);
+        database.query('DELETE FROM open_infractions WHERE infraction_id = ?', [infraction[0].infraction_id], async (err) => { if(err) console.log(err) })
+    }catch(err) {console.log(err)}
+}
 
 module.exports.run = async (bot, message, args) => {
     if(config.deleteModCommandsAfterUsage  == 'true') {
@@ -32,7 +42,14 @@ module.exports.run = async (bot, message, args) => {
         setNewModLogMessage(bot, config.defaultModTypes.unmute, message.author.id, Member.id, reason);
         publicModResponses(message, config.defaultModTypes.unmute, message.author.id, Member.id, reason);
         privateModResponse(Member, config.defaultModTypes.unmute, reason);
-        return Member.roles.remove([MutedRole]);
+        database.query(`SELECT * FROM open_infractions WHERE user_id = ? ORDER BY id DESC`, [Member.id], async (err, res) => {
+            if(err) {
+                console.log(err)
+            }
+            await deleteEntries(await res);
+            if(config.debug == 'true') console.info('Infraction Command passed!')
+            return await Member.roles.remove([MutedRole]);
+        });
     }
     catch(err) {
         console.log(err);
