87c234c9c2aebd2bc2e795d1c418852041e6a357
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-21 21:11:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,111 @@
 const Discord = require("discord.js");
 const fs = require('fs');
 const config = require('./config.json');
-const { database } = require("./src/db/db");
-const { checkInfractions } = require("./src/events/checkInfraction");
-const { checkTemproles } = require("./src/events/checkTemproles");
-const { auditLog } = require("./utils/auditlog/auditlog");
-const { autoresponse } = require("./utils/autoresponse/autoresponse");
-const { blacklist } = require("./utils/blacklist/blacklist");
+const {
+  Database
+} = require("./src/db/db");
+const {
+  checkInfractions
+} = require("./src/events/checkInfraction");
+const {
+  checkTemproles
+} = require("./src/events/checkTemproles");
+const {
+  auditLog
+} = require("./utils/auditlog/auditlog");
+const {
+  autoresponse
+} = require("./utils/autoresponse/autoresponse");
+const {
+  blacklist
+} = require("./utils/blacklist/blacklist");
 const defaultCooldown = new Set();
 
 const bot = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]
 });
+
+const database = new Database();
+
+bot.on('guildCreate', guild => {
+
+  database.query(`CREATE TABLE ${guild.id}_config LIKE _guild_config_template`).then(() => {
+    database.query(`INSERT INTO ${guild.id}_config (guild_id) VALUES (?)`, [guild.id]).catch(err => {})
+  }).catch(err => {});
+  database.query(`CREATE TABLE ${guild.id}_guild_logs LIKE _guild_logs_template`).catch(err => {});
+  database.query(`CREATE TABLE ${guild.id}_guild_modroles LIKE _guild_modroles_template`).catch(err => {})
+});
+
 bot.commands = new Discord.Collection();
 
 let modules = fs.readdirSync('./src/commands/');
 modules.forEach((module) => {
   fs.readdir(`./src/commands/${module}`, (err, files) => {
-    if(err) {
+    if (err) {
       console.log(`Mission Folder!!`, err);
     }
     files.forEach((file) => {
-      if(!file.endsWith('.js')) return;
+      if (!file.endsWith('.js')) return;
       let command = require(`./src/commands/${module}/${file}`);
       console.log(`${command.help.name} Command has been loaded!`);
-      if(command.help.name) bot.commands.set(command.help.name, command)
+      if (command.help.name) bot.commands.set(command.help.name, command)
 
     })
   });
-})
+});
 
 //When a member join add a role called Member to them and welcome them in a channel welcome
 bot.on('guildMemberAdd', member => {
-  //Log the newly joined member to console
-  console.log('User' + member.user.tag + ' has joined the server!');
+  database.query(`SELECT welcome_channel, member_role FROM ${member.guild.id}_config`).then(res => {
+    if (res !== '') {
+      bot.channels.cache.find(c => c.id === res[0].welcome_channel).send('Welcome ' + member.user.username)
 
-  //Find a channel named welcome and send a Welcome message
-  bot.channels.cache.find(c => c.name === "m-sotstis").send('Welcome ' + member.user.username)
+      let role = member.guild.roles.cache.find(r => r.id === res[0].member_role);
 
-  //Find a role called Member
-  let role = member.guild.roles.cache.find(r => r.name === 'Member');
-
-  //After 10 seconds add the member role to new user
-  setTimeout(function () {
-    member.roles.add(role);
-  }, 10000);
+      //setTimeout(function () {
+      member.roles.add(role);
+      // }, 10000);
+    }
+  }).catch(err => console.log(err))
 });
 
 //Command Manager
 bot.on("messageCreate", async message => {
-  //Check if author is a bot or the message was sent in dms and return
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+  var prefix;
+  database.query(`SELECT prefix FROM ${message.guild.id}_config`).then(res => {
+    prefix = res;
 
-  //get prefix from config and prepare message so it can be read as a command
-  let prefix = config.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
 
-  //Check for prefix & check for blacklist words
-  if (!cmd.startsWith(config.prefix)) {
-    blacklist(1, message);
-    autoresponse(message);
-  };
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
-  //Get the command from the commands collection and then if the command is found run the command file
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if (commandfile && blacklist(0, message)) {
-    if (defaultCooldown.has(message.author.id)) {
-      message.channel.send(`Wait ${config.defaultCooldown.text} before getting typing this again.`);
-      return
-    } else {
-      commandfile.run(bot, message, args);
-      
-      defaultCooldown.add(message.author.id);
-      setTimeout(async () => {
-        defaultCooldown.delete(message.author.id);
-      }, config.defaultCooldown.format);
+    auditLog(bot, message.guild.id);
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (!cmd.startsWith(prefix)) {
+      blacklist(1, message);
+      autoresponse(message);
+    };
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if (commandfile && blacklist(0, message)) {
+      if (defaultCooldown.has(message.author.id)) {
+        return message.channel.send(`Wait ${config.defaultCooldown.text} before getting typing this again.`);
+      } else {
+        defaultCooldown.add(message.author.id);
+        commandfile.run(bot, message, args);
+        setTimeout(async () => {
+          defaultCooldown.delete(message.author.id);
+        }, config.defaultCooldown.format);
+      }
     }
-  }else {
-    return;
-  }
+  }).catch(err => console.log(err));
 });
 
 bot.once('ready', () => {
-  auditLog(bot);
   checkInfractions(bot);
   checkTemproles(bot)
 
