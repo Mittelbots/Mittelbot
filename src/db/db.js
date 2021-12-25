d4528dd81322bfd8636d4e9479551214118de143
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-25 15:42:50 +0100

    changed db funtion to class

--- src/db/db.js
+++ src/db/db.js
@@ -1,26 +1,32 @@
 const { createConnection } = require('mysql');
-const dbconfig = require('./db_config.json');
 const config = require('../../config.json');
+const dbconfig = require('./db_config.json');
 
-const database = createConnection({
-  host: dbconfig.host,
-  user: dbconfig.user,
-  password: dbconfig.password,
-  database: dbconfig.database,
-});
-
-function mysql_handleDisconnect() {
-  database.connect((err) => {
-    if(err) {
-      console.log(`${config.errormessages.databaseconnectionerror}`, err);
-      setTimeout(() => {
-        mysql_handleDisconnect();
-      }, 5000);
-    }
-    console.log(`${config.successmessages.database}`);
-  });
+class Database {
+  constructor() {
+    this.connection = createConnection({
+      host: dbconfig.host,
+      user: dbconfig.user,
+      password: dbconfig.password,
+      database: dbconfig.database,
+    });
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if(err) return reject(err);
+        resolve(rows);
+      })
+    })
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err =>{
+        if(err) return reject(err);
+        resolve();
+      })
+    })
+  }
 }
 
-mysql_handleDisconnect();
-
-module.exports = { database };
+module.exports = { Database };
