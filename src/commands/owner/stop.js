725e409f8020b1c7313d16837584bbb82b6e41ef
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-30 14:13:25 +0100

    the will stop now in a good way

--- src/commands/owner/stop.js
+++ src/commands/owner/stop.js
@@ -3,7 +3,7 @@ const config = require('../../../config.json');
 module.exports.run = async (bot, message, args) => {
     if(message.author.id === config.Bot_Owner_ID) {
         message.reply(`Ok sir, Bot stopped!`);
-        bot.destroy()
+        process.exit();
     }
 }
 
