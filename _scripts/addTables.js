b681f1dc11efc938f45957ac640b4d6dbf9c18b0
Mittelblut9 <sauer.benedikt@outlook.de>
2021-12-30 15:31:28 +0100

    added script to add non existing tables to all guild

---
+++ _scripts/addTables.js
@@ -0,0 +1,23 @@
+const {Database} = require('../src/db/db');
+const tables = require('../src/db/table.json');
+
+const database = new Database();
+
+let res = database.query(`SELECT * FROM all_guild_id`).then(async res => {
+    return await res;
+}).catch(err => {
+    throw err;
+});
+
+
+res.then((res) => {
+    for (let g in res) {
+        for(let t in tables.tables) {
+            database.query(`SELECT id FROM ${res[g].guild_id}${tables.tables[t]}`).catch(err => {
+                database.query(`CREATE TABLE ${res[g].guild_id}${tables.tables[t]} LIKE ${tables.tables[t]}_template`).catch(err => {throw err})
+                console.log(`${res[g].guild_id}${tables.tables[t]} added!`);
+            });
+        }
+        console.log(`${Number(g) + 1} of ${res.length} done`);
+    }
+});
