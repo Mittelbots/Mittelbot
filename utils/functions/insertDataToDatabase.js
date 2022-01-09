90d4414b12c370d938512dc88ee864a648b0651d
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-09 19:08:41 +0100

    added guild_id to "insertDataToOpenInfraction"

--- utils/functions/insertDataToDatabase.js
+++ utils/functions/insertDataToDatabase.js
@@ -6,8 +6,8 @@ function insertDataToClosedInfraction (uid, modid, mute, ban, warn, kick, till_d
     database.query('INSERT INTO closed_infractions (user_id, mod_id, mute, ban, warn, kick, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id]).catch(err => console.log(err));
 }
 
-function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id) {
-    database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, till_date, reason, infraction_id]).catch(err => console.log(err));
+function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id, gid) {
+    database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, till_date, reason, infraction_id, guild_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, till_date, reason, infraction_id, gid]).catch(err => console.log(err));
 }
 
 function inserDataToTemproles (uid, role_id, till_date, infraction_id) {
