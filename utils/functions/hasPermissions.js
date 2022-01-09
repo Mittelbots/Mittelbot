1a3a9a0687bd8083bb52970f6ea6e97eb7b4761b
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-09 16:30:35 +0100

    Update hasPermissions.js

--- utils/functions/hasPermissions.js
+++ utils/functions/hasPermissions.js
@@ -8,9 +8,9 @@ async function hasPermission(message, adminOnly, modOnly) {
         modroles = await res;
         for (let i in modroles) {
             let roleid = modroles[i].role_id;
-            let admin = modroles[i].admin;
-            let mod = modroles[i].mod;
-            let helper = modroles[i].helper;
+            let admin = modroles[i].isadmin;
+            let mod = modroles[i].ismod;
+            let helper = modroles[i].ishelper;
 
             if(adminOnly && message.member.roles.cache.find(r => r.id === roleid) && mod == 1 || helper == 1) continue;
             if(modOnly && message.member.roles.cache.find(r => r.id === roleid) && helper == 1) {continue};
