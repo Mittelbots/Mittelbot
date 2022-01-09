e2b2775d3050d123bb94789a098015dd84704fac
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-09 16:30:02 +0100

    Create isMod.js

---
+++ utils/functions/isMod.js
@@ -0,0 +1,17 @@
+async function isMod(member, message) {
+    const {Database} = require('../../src/db/db');
+    const database = new Database();
+
+    return database.query(`SELECT * FROM ${message.guild.id}_guild_modroles`).then(async (res) => {
+        var isMod = false
+        for (let i in await res) {
+            if(member.roles.cache.find(r => r.id === res[i].role_id) !== undefined) {
+                isMod = true;
+                break;
+            }
+        }
+        return isMod;
+    }).catch(err => console.log(err))
+}
+
+module.exports = {isMod}
