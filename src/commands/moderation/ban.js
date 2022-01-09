b4c9ae3f45bbd491ffcfb8783b125428c686d4af
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-09 19:33:54 +0100

    switched time & reason; added guild_id to open_infraction query

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -29,12 +29,12 @@ module.exports.run = async (bot, message, args) => {
         if (Member.id === message.author.id) return message.reply(`You can't ban yourself.`);
         if (Member.id === bot.user.id) return message.reply(`You cant't ban me.`);
     }catch(err) {
-        return message.reply(`This user is not on the server. Try to manually ban him.`);
+        return message.reply(`I can't find this user!`);
     }
 
     if (await isMod(Member, message)) return message.channel.send(`<@${message.author.id}> You can't ban a Moderator!`)
     
-    let reason = args.slice(1).join(" ");
+    let reason = args.slice(2).join(" ");
     if(!reason) return message.channel.send('Please add a reason!');
 
     if(Member.user.bot) message.reply(`Do you really want to ban <@${Member.user.id}>? It's a Bot.`).then(() => {      
@@ -64,8 +64,9 @@ module.exports.run = async (bot, message, args) => {
         });
     });
 
-    let time = args.slice(2).join(" ");
+    let time = args.slice(1).join(" ");
     reason = reason.replace(time, '');
+    time = time.replace(reason, '');
     if(time !== '') {
         var dbtime = getModTime(time);
         if(!dbtime) return message.reply(`Invalid Time [m, h, d]`);
@@ -88,7 +89,7 @@ module.exports.run = async (bot, message, args) => {
         }
 
         try {
-            database.query(`INSERT INTO open_infractions (user_id, mod_id, ban, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?)`, [Member.id, message.author.id, 1, futuredate, reason, Math.random().toString(16).substr(2, 20)]).then(async () => {
+            database.query(`INSERT INTO open_infractions (user_id, mod_id, ban, till_date, reason, infraction_id, guild_id) VALUES (?, ?, ?, ?, ?, ?, ?)`, [Member.id, message.author.id, 1, futuredate, reason, Math.random().toString(16).substr(2, 20), message.guild.id]).then(async () => {
                 await setNewModLogMessage(bot, config.defaultModTypes.ban, message.author.id, Member.id, reason, time, message.guild.id);
                 await publicModResponses(message, config.defaultModTypes.ban, message.author.id, Member.id, reason, time);
                 await privateModResponse(Member, config.defaultModTypes.ban, reason, time);
