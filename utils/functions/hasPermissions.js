e9c4d1cc7e92acd7126168a14833deb11f7adaef
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-11 17:52:58 +0100

    fixed permission bug for helper

--- utils/functions/hasPermissions.js
+++ utils/functions/hasPermissions.js
@@ -1,21 +1,19 @@
 async function hasPermission(message, adminOnly, modOnly) {
     const {Database} = require('../../src/db/db');
     const database = new Database();
-    var modroles;
 
     return database.query(`SELECT * FROM ${message.guild.id}_guild_modroles`).then(async (res) => {
         var hasPermission = false
-        modroles = await res;
-        for (let i in modroles) {
-            let roleid = modroles[i].role_id;
-            let admin = modroles[i].isadmin;
-            let mod = modroles[i].ismod;
-            let helper = modroles[i].ishelper;
+        for (let i in await res) {
+            let roleid = res[i].role_id;
+            let admin = res[i].isadmin;
+            let mod = res[i].ismod;
+            let helper = res[i].ishelper;
 
-            if(adminOnly && message.member.roles.cache.find(r => r.id === roleid) && mod == 1 || helper == 1) continue;
+            if(adminOnly && message.member.roles.cache.find(r => r.id === roleid) && (mod == 1 || helper == 1)) continue;
             if(modOnly && message.member.roles.cache.find(r => r.id === roleid) && helper == 1) {continue};
-            
             if (message.member.roles.cache.find(r => r.id === roleid)) {
+                
                 hasPermission = true;
                 break;
             }
