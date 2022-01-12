22acb8d113b7eb8e76ace975546371fc59208117
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-12 18:24:25 +0100

    added async to function

--- utils/functions/insertDataToDatabase.js
+++ utils/functions/insertDataToDatabase.js
@@ -2,15 +2,15 @@ const { Database } = require("../../src/db/db");
 
 const database = new Database;
 
-function insertDataToClosedInfraction (uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id) {
+async function insertDataToClosedInfraction (uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id) {
     database.query('INSERT INTO closed_infractions (user_id, mod_id, mute, ban, warn, kick, till_date, reason, infraction_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, warn, kick, till_date, reason, infraction_id]).catch(err => console.log(err));
 }
 
-function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id, gid) {
+async function insertDataToOpenInfraction (uid, modid, mute, ban, till_date, reason, infraction_id, gid) {
     database.query('INSERT INTO open_infractions (user_id, mod_id, mute, ban, till_date, reason, infraction_id, guild_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[uid, modid, mute, ban, till_date, reason, infraction_id, gid]).catch(err => console.log(err));
 }
 
-function inserDataToTemproles (uid, role_id, till_date, infraction_id, gid) {
+async function inserDataToTemproles (uid, role_id, till_date, infraction_id, gid) {
     database.query('INSERT INTO temproles (user_id, role_id, till_date, infraction_id, guild_id) VALUES (?, ?, ?, ?, ?)', [uid, role_id, till_date, infraction_id, gid]).catch(err => console.log(err));
 }
 
