c3e7309a9d9dc5060b3505ae8503957a2457cb3b
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-13 22:27:04 +0100

    fixed #52

--- utils/privatResponses/privateModResponses.js
+++ utils/privatResponses/privateModResponses.js
@@ -1,16 +1,21 @@
 const { MessageEmbed } = require("discord.js");
 async function privateModResponse(member, type, reason, time) {
-    var privateModMessage = new MessageEmbed()
-    .setTitle(`**You got ${type}!**`)
-    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
-    .setTimestamp();
+    try {
+        var privateModMessage = new MessageEmbed()
+        .setTitle(`**You got ${type}!**`)
+        .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+        .setTimestamp();
 
-    if(time) {
-        privateModMessage.addField(`Time`, `**${time}** `);
+        if(time) {
+            privateModMessage.addField(`Time`, `**${time}** `);
+        }
+
+        member.send({embeds: [privateModMessage]});
+        return;
+    }catch(err) {
+        //DM'S ARE NO OPEN OR THE USER BLOCKED THE BOT
+        return;
     }
-
-    member.send({embeds: [privateModMessage]});
-    return;
 }
 
 module.exports = {privateModResponse}
