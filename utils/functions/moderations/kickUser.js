8647c9ca495ffbea204eb0f0670c1155a7eada70
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-18 22:07:04 +0100

    Create kickUser.js

---
+++ utils/functions/moderations/kickUser.js
@@ -0,0 +1,26 @@
+const { log } = require("../../../logs");
+const { setNewModLogMessage } = require("../../modlog/modlog");
+const { privateModResponse } = require("../../privatResponses/privateModResponses");
+const { publicModResponses } = require("../../publicResponses/publicModResponses");
+const { createInfractionId } = require("../createInfractionId");
+const { errorhandler } = require("../errorhandler/errorhandler");
+const { insertDataToClosedInfraction } = require("../insertDataToDatabase");
+
+async function kickUser(bot, member, message, config, reason) {
+    try {
+        insertDataToClosedInfraction(member.id, message.author.id, 0, 0, 0, 1, null, reason, createInfractionId())
+        await setNewModLogMessage(bot, config.defaultModTypes.kick, message.author.id, member.id, reason, null, message.guild.id);
+        await publicModResponses(message, config.defaultModTypes.kick, message.author.id, member.id, reason);
+        await privateModResponse(member, config.defaultModTypes.kick, reason);
+        setTimeout(async () => {
+            if(config.debug == 'true') console.info('Kick Command passed!')
+            return member.kick({
+                reason: reason
+            });;
+        }, 500);
+    } catch (err) {
+        return errorhandler(err, config.errormessages.botnopermission, message.channel, log, config)
+    }
+}
+
+module.exports = {kickUser}
