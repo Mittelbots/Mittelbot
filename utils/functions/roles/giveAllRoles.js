43eda8bf49559baff8e6698887fbff47c26515c7
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-18 22:08:26 +0100

    fixed bug

--- utils/functions/roles/giveAllRoles.js
+++ utils/functions/roles/giveAllRoles.js
@@ -7,11 +7,11 @@
 
 const config = require('../../../config.json');
 
-async function giveAllRoles(user, guild, roles) {
+async function giveAllRoles(user, guild, roles, bot) {
     for (let x in roles) {
-        let r = await guild.roles.cache.find(role => role.id == roles[x])
+        let r = await bot.guilds.cache.get(guild).roles.cache.find(role => role.id == roles[x])
         if(config.debug == 'true') console.log(r.name);
-        await user.roles.add([await r]);
+        await bot.guilds.cache.get(guild).members.cache.get(user).roles.add([await r]);
     }
 }
 
