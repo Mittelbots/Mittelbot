5ef8012a420e97f8283d9b6aaa2cbfcbf91564f4
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-24 17:45:55 +0100

    bug fix
    - fixed bug where the bot crashed when it tries to send a dm to close dm user

--- utils/privatResponses/privateModResponses.js
+++ utils/privatResponses/privateModResponses.js
@@ -3,22 +3,18 @@ const { generateModEmote } = require("../functions/generateModEmote");
 const config = require('../../config.json');
 
 async function privateModResponse(member, type, reason, time, bot) {
-    try {
-        var privateModMessage = new MessageEmbed()
-        .setTitle(`${await generateModEmote(config, bot, type)} **You got ${type}!**`)
-        .addField(`Reason`, `${reason || "No Reason Provided!"}`)
-        .setTimestamp();
-
-        if(time) {
-            privateModMessage.addField(`Time`, `**${time}** `);
-        }
-
-        member.send({embeds: [privateModMessage]});
-        return;
-    }catch(err) {
-        //DM'S ARE NO OPEN OR THE USER BLOCKED THE BOT
-        return;
+    var privateModMessage = new MessageEmbed()
+    .setTitle(`${await generateModEmote(config, bot, type)} **You got ${type}!**`)
+    .addField(`Reason`, `${reason || "No Reason Provided!"}`)
+    .setTimestamp();
+    if(time) {
+        privateModMessage.addField(`Time`, `**${time}** `);
     }
+
+    return member.send({embeds: [privateModMessage]}).catch(err => {
+        /** MEMBER HAS DM CLOSED */
+        return false;
+    });
 }
 
 module.exports = {privateModResponse}
