84490997d0c175283b1bfd118d45fb35d0b9483f
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-29 22:42:51 +0100

    added a few things
    - added databse interaction instead of using .json file
    - added delete & view command

--- src/commands/admin/scam.js
+++ src/commands/admin/scam.js
@@ -1,6 +1,5 @@
 const config = require('../../../config.json');
 const commandconfig = require('../../../command_config.json');
-const advancedScamList = require('../../../advancedScamList.json')
 
 const {
     MessageEmbed,
@@ -14,6 +13,10 @@ const dns = require('dns');
 const url = require('url');
 
 const fs = require('fs')
+
+const {Database} = require('../../db/db');
+const { errorhandler } = require('../../../utils/functions/errorhandler/errorhandler');
+const { log } = require('../../../logs');
 
 module.exports.run = async (bot, message, args) => {
 
@@ -31,9 +34,7 @@ module.exports.run = async (bot, message, args) => {
 
     if(setting === commandconfig.scam.add.command) {
 
-        for(let i in advancedScamList.requestblacklist) {
-            if(advancedScamList.requestblacklist[i] === message.guild.id) return message.reply('Your server is on request blacklist! You can\'t sent any requests until the bot moderators removes your server from it.');
-        }
+        const database = new Database();
 
         var value = args[1];
 
@@ -42,7 +43,23 @@ module.exports.run = async (bot, message, args) => {
         if(value.search('http://') !== -1 && value.search('https://') !== -1) {
             value = `http://${value}/`;
         }
-        console.log(value)
+
+        var pass = false;
+        await database.query(`SELECT * FROM advancedScamList`).then(res => {
+            for(let i in res) {
+                if(res[i].guild_id === message.guild.id) {
+                    pass = false;
+                    return message.reply('Your server is on blacklist! You can\'t sent any requests until the bot moderators removes your server from it.');
+                }
+                if(res[i].link === removeHttp(value)) {
+                    pass = false;
+                    return message.reply(`This URL already exits in current Scamlist`)
+                }
+                pass = true;
+            }
+        });
+        if(!pass) return database.close();
+
         const parsedLookupUrl = url.parse(value);
 
         dns.lookup(parsedLookupUrl.protocol ? parsedLookupUrl.host : parsedLookupUrl.path, async (err, address, family) => {
@@ -56,10 +73,6 @@ module.exports.run = async (bot, message, args) => {
 
                 const blacklist = 'blacklist';
                 const blacklistLabel = 'Blacklist Server';
-
-                for(let i in advancedScamList.link) {
-                    if(advancedScamList.link[i] === value) return message.reply('This URL already exits');
-                }
 
                 const newScamLinkembed = new MessageEmbed()
                     .setTitle('New Advanced ScamList request')
@@ -97,18 +110,12 @@ module.exports.run = async (bot, message, args) => {
                 collector.on('collect', async interaction => {
                     interaction.deferUpdate();
                     if(interaction.customId === accept) {
-                        advancedScamList.link.push(value)
-                        if(advancedScamList.length <= 0) return message.reply('Something went wrong. Data is empty!');
-                        fs.writeFile('./advancedScamList.json', JSON.stringify(advancedScamList), (err) => console.log(err))
-                        
+                        database.query(`INSERT INTO advancedScamList (link) VALUES (?)`, [value]).catch(err => errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config));
                         return await message.author.send(`Your ScamList request was accepted! \n Link: \`${value}\` `).catch(err => {})
                     }else if(interaction.customId === deny) {
                         return await message.author.send(`Your ScamList request was denied! \n Link: \`${value}\` `).catch(err => {})
                     }else {
-                        advancedScamList.requestblacklist.push(message.guild.id);
-                        if(advancedScamList.length <= 0) return message.reply('Something went wrong. Data is empty!');
-                        fs.writeFile('./advancedScamList.json', JSON.stringify(advancedScamList), (err) => console.log(err))
-
+                        database.query(`INSERT INTO advancedScamList (guild_id) VALUES (?)`, [message.guild.id]).catch(err => errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config));
                         return await message.author.send(`Your Server got added to the blacklist!`).catch(err => {})
                     }
                 });
@@ -122,6 +129,8 @@ module.exports.run = async (bot, message, args) => {
                             buttons.components[i].setDisabled(true)
                         }
                         sentMessage.edit({embeds: [newScamLinkembed], components: [buttons]})
+
+                        database.close();
                     });
                     
                     return;
@@ -129,11 +138,198 @@ module.exports.run = async (bot, message, args) => {
                 
             } else {
                 //! URL IS INVALID
+                database.close();
                 return message.reply('Invalid URL!');
             }
         });
 
+
+    }else if(setting === commandconfig.scam.delete.command || setting === commandconfig.scam.delete.alias) {
+        const database = new Database();
+
+        var value = args[1];
+
+        if(value == undefined) return message.reply('Please add an valid URL');
+
+        if(value.search('http://') !== -1 && value.search('https://') !== -1) {
+            value = `http://${value}/`;
+        }
+
+        var exits = false
+        var pass = false;
+        await database.query(`SELECT * FROM advancedScamList`).then(res => {
+            for(let i in res) {
+                if(res[i].guild_id === message.guild.id) {
+                    pass = false;
+                    return message.reply('Your server is on blacklist! You can\'t sent any requests until the bot moderators removes your server from it.');
+                }else if(res[i].link === removeHttp(value)) {
+                    exits = true
+                    pass = true;
+                };
+            }
+        });
+        if(!pass) return database.close();
+        if(!exits) { 
+            message.reply('This URL doesn\'t exits in current list!');
+            return database.close();
+        } 
+
+        const accept = 'accept';
+        const deny = 'deny';
+
+        const blacklist = 'blacklist';
+        const blacklistLabel = 'Blacklist Server';
+
+        const newScamLinkembed = new MessageEmbed()
+        .setTitle('New **DELETE** Scam Link request')
+        .addField('**LINK:**', `\n${removeHttp(value)}`)
+        .addField('**VIEW SCAN**', `https://www.urlvoid.com/scan/${value}/`)
+        .addField('**SERVER:**', `\n${message.guild.id} (${message.guild.name})`)
+        .setTimestamp()
+
+        const buttons = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId(accept)
+                    .setLabel(accept)
+                    .setStyle('PRIMARY')                        
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId(deny)
+                    .setLabel(deny)
+                    .setStyle('PRIMARY')                        
+            )
+            .addComponents(
+                new MessageButton()
+                    .setCustomId(blacklist)
+                    .setLabel(blacklistLabel)
+                    .setStyle('DANGER')                        
+            )
+
+        const sentMessage = await bot.guilds.cache.get(config.DEVELOPER_DISCORD_GUILD_ID).channels.cache.get('937032777583427586').send({embeds: [newScamLinkembed], components: [buttons]})
+
+        const collector = sentMessage.createMessageComponentCollector({
+            max: 1
+        });
+
+        collector.on('collect', async interaction => {
+            interaction.deferUpdate();
+            if(interaction.customId === accept) {
+                database.query(`DELETE FROM advancedScamList WHERE link = ?`, [removeHttp(value)]).catch(err => {
+                    errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config)
+                    return database.close();
+                })
+                return await message.author.send(`Your ScamList request was accepted! \n Link: \`${value}\` `).catch(err => {})
+            }else if(interaction.customId === deny) {
+                return await message.author.send(`Your ScamList request was denied! \n Link: \`${value}\` `).catch(err => {})
+            }else {
+                database.query(`INSERT INTO advancedScamList (guild_id) VALUES (?)`, [message.guild.id]).catch(err => {
+                    errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config)
+                    return database.close();
+                });
+                return await message.author.send(`Your Server got added to the blacklist!`).catch(err => {})
+            }
+        });
+
+        collector.on('end', (collected, reason) => {
+            collected.forEach(x => {
+                for(let i in buttons.components) {
+                    if(buttons.components[i].customId === x.customId) {
+                        buttons.components[i].setStyle('SUCCESS');
+                    }
+                    buttons.components[i].setDisabled(true)
+                }
+                sentMessage.edit({embeds: [newScamLinkembed], components: [buttons]})
+            });
+
+            return database.close();
+        });
+    }else if(setting === commandconfig.scam.view.command) {
+        var value = args[1];
+        
+        const database = new Database();
+
+        if(value === undefined) {
+            database.query(`SELECT * FROM advancedScamList WHERE link != ''`).then(async res => {
+                const backId = 'back'
+                const forwardId = 'forward'
+                const backButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Back',
+                emoji: '⬅️',
+                customId: backId
+                });
+                const forwardButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Forward',
+                emoji: '➡️',
+                customId: forwardId
+                });
+
+                const embedMessage = new MessageEmbed()
+                .setTitle('Showing all current blacklist Links')
+
+                const generateEmbed = async start => {
+                    for(i in res) {
+                        if(i === (Number(start) + Number(30))) return;
+                        embedMessage.addField('LINK:', res[start].link)
+                    }
+                    return embedMessage;
+                }
+
+                const canFitOnOnePage = res.length <= 30;
+                const sentMessage = await message.channel.send({
+                    embeds: [await generateEmbed(0)],
+                    components: canFitOnOnePage ? [] : [new MessageActionRow({components: [forwardButton]})]
+                });
+
+                if(canFitOnOnePage) return;
+
+                const collector = sentMessage.createMessageComponentCollector({
+                    filter: ({user}) => user.id === message.author.id
+                });
+
+                let currentIndex = 0;
+                collector.on('collect', async interaction => {
+                    interaction.customId === backId ? (currentIndex -= 10) : (currentIndex += 10)
+
+                    await interaction.update({
+                        embeds: [await generateEmbed(currentIndex)],
+                        components: [
+                            new MessageActionRow({
+                                components: [
+                                    ...(currentIndex ? [backButton] : []),
+                                    ...(currentIndex + 10 < data.length ? [forwardButton] : [])
+                                ]
+                            })
+                        ]
+                    });
+                });
+
+            }).catch(err => {
+                database.close();
+                return errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config);
+            })
+        }else {
+            value = removeHttp(value);
+            const database = new Database();
+
+            database.query(`SELECT link FROM advancedScamList WHERE link = ?`, [value]).then(res => {
+                if(res.length <= 0) {
+                    database.close();
+                    return message.reply('❌ **No results by searching this URL**');
+                }
+
+                database.close()
+                return message.reply('✅ **Matching link found!**');
+            }).catch(err => {
+                database.close()
+                return errorhandler(err, config.errormessages.databasequeryerror, bot.guilds.cache.get(interaction.guildId).channels.cache.get(interaction.channelId), log, config);
+            })
+        }
     }
+    else return;
 }
 
 
