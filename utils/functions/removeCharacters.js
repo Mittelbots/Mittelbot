24e1efbf024b360460bd2093e094d795a33df999
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-29 19:13:22 +0100

    added "removehttp" function

--- utils/functions/removeCharacters.js
+++ utils/functions/removeCharacters.js
@@ -2,4 +2,13 @@ function removeMention(mention) {
     return mention = mention.replaceAll('<', '').replaceAll('@', '').replaceAll('!', '').replaceAll('&', '').replaceAll('>', '');
 }
 
-module.exports = {removeMention}
+function removeHttp(link) {
+    link = link.replaceAll('/', '').replaceAll(':', '')
+    if(link.search('http://') !== -1) {return link = link.replace('http', '');}
+    else { return link = link.replace('https', ''); }
+}
+
+module.exports = {
+    removeMention,
+    removeHttp
+}
