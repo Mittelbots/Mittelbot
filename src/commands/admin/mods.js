be0e4f1dea8dde0d1628f2a061769d23dd51fbfd
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-29 15:33:33 +0100

    reworked mod role settings
    - now with user friendly buttons

--- src/commands/admin/mods.js
+++ src/commands/admin/mods.js
@@ -1,14 +1,20 @@
 const config = require('../../../config.json');
 const commandconfig = require('../../../command_config.json');
 const {
-    Permissions
+    Permissions,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton 
 } = require('discord.js');
 const {
     viewSetting
 } = require('../../../utils/functions/viewSetting');
 const { log } = require('../../../logs');
 
-const {Database} = require('../../db/db')
+const {Database} = require('../../db/db');
+const { errorhandler } = require('../../../utils/functions/errorhandler/errorhandler');
+const { removeMention } = require('../../../utils/functions/removeCharacters');
+const { insertPermsToModroles, deletePermsFromModroles, updatePermsFromModroles } = require('../../../utils/functions/insertDataToDatabase');
 
 const database = new Database();
 
@@ -26,57 +32,156 @@ module.exports.run = async (bot, message, args) => {
     let setting = args[0]
 
 
-    //? ADD OR REMOVE MODROLES
+    //? ADD OR REMOVE MODROLES //
     if (setting === commandconfig.mods.modroles.command || setting === commandconfig.mods.modroles.alias) {
 
         let value = args[1];
-        let type = args.slice(2).join(" ");
 
-        if (value == '') return message.reply(`You have to mention a role!`);
-        if (type == '') return message.reply(`You have to pass options! (exp.: ${config.defaultprefix}${commandconfig.mods.modroles.command} ${setting} ${value || '@role'} **true false false**)`)
+        const isadmin = 'isadmin';
+        const isadminLabel = 'Admin';
+        var db_isadmin;
 
-        value = value.replaceAll('<', '').replaceAll('@', '').replaceAll('&', '').replaceAll('>', '');
+        const ismod = 'ismod';
+        const ismodLabel = 'Moderator';
+        var db_ismod;
 
-        if (!message.guild.roles.cache.get(value)) return message.reply(`<@&${value[i]}> not found`);
+        const ishelper = 'ishelper';
+        const ishelperLabel = 'Helper';
+        var db_ishelper;
+
+        const remove = 'remove';
+        const removeLabel = 'Remove';
 
 
-        const database = new Database();
+        var roleid;
 
-        let x = await database.query(`SELECT * FROM ${message.guild.id}_guild_modroles`).then(res => {
-            if (res.length > 0) {
-                for (let i in res) {
-                    if (value === res[i].role_id) {
-                        message.reply(`<@&${value}> is already a Mod role. It will be removed!`);
-                        database.query(`DELETE FROM ${message.guild.id}_guild_modroles WHERE role_id = ?`, [res[i].role_id])
-                        return false;
-                    }
-                }
+        var status = false;
+
+        args[1] = removeMention(args[1])
+
+        await database.query(`SELECT * FROM ${message.guild.id}_guild_modroles WHERE role_id = ?`, [args[1]]).then(async res => {
+            if(await res.length > 0) {
+                roleid = await res[0].role_id;
+                db_isadmin = await res[0].isadmin
+                db_ismod = await res[0].ismod
+                db_ishelper = await res[0].ishelper
+                status = true;
             }
         }).catch(err => {
-            log.fatal(err);
-            if(config.debug == 'true') console.log(err);
-            return message.channel.send(`${config.errormessages.databasequeryerror}`);
+            errorhandler(err, config.errormessages.databasequeryerror, message.channel, log, config);
         });
+        
+        if(roleid == null) value = removeMention(value);
+        else value = roleid;
 
-        if(await x === false) return;
+        const role = await bot.guilds.cache.get(message.guild.id).roles.fetch(value)
 
-        type = type.split(' ');
+        let modroleembed = new MessageEmbed()
+        .setTitle(`Choose setting for _${role.name}_. \n\nCurrent: **${(db_isadmin) ? 'Admin': (db_ismod) ? 'Moderator': (db_ishelper) ? 'Helper' : 'Not set yet'}**`)
 
-        for (let i in type) {
-            if (type[i] != '1' && type[i] != '0' && type[i].toLowerCase() != 'true' && type[i].toLowerCase() != 'false') return message.reply(`Wrong options! Only 0/1 or true or false allowed.`)
-            if (type[i].toLowerCase() == 'true') type[i] = '1';
-            if (type[i].toLowerCase() == 'false') type[i] = '0';
+        const row = new MessageActionRow()
+
+        if(db_isadmin !== 1){
+            row.addComponents(
+                new MessageButton()
+                .setCustomId(isadmin)
+                .setLabel(isadminLabel)
+                .setStyle('PRIMARY')
+            );
+            modroleembed.addField('Admin Permissions', 'This role can use commands like ban, unban, etc.', true)
+        }
+        if(db_ismod !== 1) {
+            row.addComponents(
+                new MessageButton()
+                .setCustomId(ismod)
+                .setLabel(ismodLabel)
+                .setStyle('PRIMARY')
+            );
+            modroleembed.addField('Moderator Permissions', 'This role can use commands like mute, kick, etc.', true)
+        }
+        if(db_ishelper !== 1) {
+            row.addComponents(
+                new MessageButton()
+                .setCustomId(ishelper)
+                .setLabel(ishelperLabel)
+                .setStyle('PRIMARY')
+            );
+            modroleembed.addField('Helper Permissions', 'This role can use commands like warn, mute, etc.', true)
+        }
+        if(db_isadmin !== undefined || db_ismod !== undefined || db_ishelper !== undefined) {
+            row.addComponents(
+                new MessageButton()
+                .setCustomId(remove)
+                .setLabel(removeLabel)
+                .setStyle('DANGER')
+            )
         }
 
-        if(type[0] == ' ') return message.reply(`You have an space betweeen your role mention and the options!`)
+        
+        const embedMessage = await message.reply({embeds: [modroleembed], components: [row]});
 
-        database.query(`INSERT INTO ${message.guild.id}_guild_modroles (role_id, isadmin, ismod, ishelper) VALUES (?, ?, ?, ?)`, [value, type[0], type[1], type[2]]).then(() => {
-            return message.reply(`<@&${value}> successfully saved as a new Modrole!`);
-        }).catch(err => {
-            log.fatal(err);
-            if(config.debug == 'true') console.log(err);
-            return message.channel.send(`${config.errormessages.databasequeryerror}`);
+        const collector = embedMessage.createMessageComponentCollector({
+            filter: ({user}) => user.id === message.author.id,
+            time: 15000,
+            max: 1
         });
+
+        collector.on('collect', async interaction => {
+            interaction.deferUpdate();
+            if(interaction.customId === isadmin) {
+                if(db_isadmin == 1) return;
+                if(status) {
+                    return await updatePermsFromModroles(message.guild.id, value, 1, 0, 0)
+                }
+                else {
+                    return await insertPermsToModroles(message.guild.id, value, 1, 0, 0) 
+                }
+            }else if(interaction.customId === ismod) {
+                if(db_ismod == 1) return;
+                if(status) {
+                    return await updatePermsFromModroles(message.guild.id, value, 0, 1, 0)
+                }
+                else {
+                    return await insertPermsToModroles(message.guild.id, value, 0, 1, 0) 
+                }
+            } else if(interaction.customId === ishelper) {
+                if(db_ishelper == 1) return;
+                if(status){
+                    return await updatePermsFromModroles(message.guild.id, value, 0, 0, 1)
+                }
+                else{
+                    return await insertPermsToModroles(message.guild.id, value, 0, 0, 1)
+                }
+            } else {
+                return await deletePermsFromModroles(message.guild.id, value)
+            }
+        });
+
+        collector.on('end', (collected, reason) => {
+            if(reason == 'time'){
+                for(let i in row.components) {
+                    row.components[i].setDisabled(true)
+                }
+                embedMessage.edit({content: '**Time limit reached**', embeds: [modroleembed], components: [row]})
+            }
+            collected.forEach(x => {
+                for(let i in row.components) {
+                    if(row.components[i].customId === x.customId) {
+                        row.components[i].setStyle('SUCCESS');
+                    }else {
+                        row.components[i].setStyle('SECONDARY');
+                    }
+                    row.components[i].setDisabled(true)
+                }
+                embedMessage.edit({embeds: [modroleembed], components: [row]})
+            });
+            
+            return;
+        });
+
+
+        return;
+
     } else return;
 }
 
