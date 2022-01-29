1048f7c102bd81628590872c0ab342c8113a569d
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-29 15:59:06 +0100

    Update mods.js

--- src/commands/admin/mods.js
+++ src/commands/admin/mods.js
@@ -118,7 +118,7 @@ module.exports.run = async (bot, message, args) => {
         }
 
         
-        const embedMessage = await message.reply({embeds: [modroleembed], components: [row]});
+        const embedMessage = await message.channel.send({embeds: [modroleembed], components: [row]});
 
         const collector = embedMessage.createMessageComponentCollector({
             filter: ({user}) => user.id === message.author.id,
@@ -160,6 +160,7 @@ module.exports.run = async (bot, message, args) => {
         collector.on('end', (collected, reason) => {
             if(reason == 'time'){
                 for(let i in row.components) {
+                    row.components[i].setStyle('DANGER');
                     row.components[i].setDisabled(true)
                 }
                 embedMessage.edit({content: '**Time limit reached**', embeds: [modroleembed], components: [row]})
