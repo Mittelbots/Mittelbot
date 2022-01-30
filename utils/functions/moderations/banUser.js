bdfd1c5a75dc979517c52095fbe3bdc3596c9163
Mittelblut9 <sauer.benedikt@outlook.de>
2022-01-30 19:28:52 +0100

    Update banUser.js

--- utils/functions/moderations/banUser.js
+++ utils/functions/moderations/banUser.js
@@ -12,19 +12,18 @@ async function banUser(database, member, message, reason, bot, config, log, dbti
     else mod = message.author;
 
     let infid = createInfractionId(database);
-    let pass = false
 
     if(config.debug == 'true') console.info('Ban Command passed!');
-    let x = await member.ban({reason: reason}).then(() => {pass = true}).catch(err => {
+    insertDataToOpenInfraction(member.id, mod.id, 0, 1, getFutureDate(dbtime), reason, infid, message.guild.id, null)
+    setNewModLogMessage(bot, config.defaultModTypes.ban, mod.id,  member.id, reason, time, message.guild.id, database);
+    publicModResponses(message, config.defaultModTypes.ban, mod, member.id, reason, time, bot);
+    privateModResponse(member, config.defaultModTypes.ban, reason, time, bot, message.guild.name)
+
+    return member.ban({reason: reason}).catch(err => {
         return database.query(`DELETE FROM open_infractions WHERE infraction_id = ?`, [infid]).catch(err => {
-            pass = false;
             return errorhandler(err, config.errormessages.databasequeryerror, message.channel, log, config);
         });
     });
-    await insertDataToOpenInfraction(member.id, mod.id, 0, 1, getFutureDate(dbtime), reason, infid, message.guild.id, null)
-    await setNewModLogMessage(bot, config.defaultModTypes.ban, mod.id,  member.id, reason, time, message.guild.id, database);
-    await publicModResponses(message, config.defaultModTypes.ban, mod, member.id, reason, time, bot);
-    await privateModResponse(member, config.defaultModTypes.ban, reason, time, bot, message.guild.name)
 }
 
 module.exports = {banUser};
