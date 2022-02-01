a716b573d2445598892430f87364721033f8b394
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-01 18:21:58 +0100

    cleared some stuff

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const Discord = require("discord.js");
 const fs = require('fs');
 
 const config = require('./src/assets/json/_config/config.json');
-const {
-  Database
-} = require("./src/db/db");
+
 const {
   checkInfractions
 } = require("./src/events/checkInfraction");
@@ -14,8 +12,7 @@ const {
 const {
   auditLog
 } = require("./utils/auditlog/auditlog");
-const { gainXP } = require("./src/events/levelsystem/levelsystem");
-const { guildCreate } = require("./src/events/guildCreate/guildCreate");
+
 const { log } = require('./logs');
 const { errorhandler } = require('./utils/functions/errorhandler/errorhandler');
 // const {
@@ -25,18 +22,18 @@ const { errorhandler } = require('./utils/functions/errorhandler/errorhandler');
 //   blacklist
 // } = require("./utils/blacklist/blacklist");
 
-const { giveAllRoles } = require("./utils/functions/roles/giveAllRoles");
-const { getAllRoles } = require("./utils/functions/roles/getAllRoles");
-const { checkForScam } = require("./utils/checkForScam/checkForScam");
+
 const { deployCommands } = require("./utils/functions/deployCommands/deployCommands");
 
-const lvlconfig = require('./src/assets/json/levelsystem/levelsystem.json');
-const whitelist = require('./src/assets/json/whitelist/whitelist.json');
+
+
 const token = require('./_secret/token.json');
 
-const defaultCooldown = new Set();
-const settingsCooldown = new Set();
-const levelCooldown = new Set();
+const { guildCreate } = require("./bot/guildCreate");
+const { messageCreate } = require("./bot/messageCreate");
+const { guildMemberAdd } = require("./bot/guildMemberAdd");
+const {guildMemberRemove} = require("./bot/guildMemberRemove");
+const { Database } = require("./src/db/db");
 
 const bot = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_VOICE_STATES"],
@@ -49,171 +46,27 @@ const bot = new Discord.Client({
 });
 bot.setMaxListeners(10);
 
-
-bot.on('guildCreate', async (guild) => await guildCreate(database, guild, whitelist, log));
+bot.on('guildCreate', async (guild) => {
+  return await guildCreate(guild)}
+);
 
 bot.commands = new Discord.Collection();
 
-deployCommands(fs, log, config, bot);
+deployCommands(bot);
 
-bot.on('guildMemberAdd', member => {
-  const database = new Database();
-
-  database.query(`SELECT * FROM ${member.guild.id}_guild_member_info WHERE user_id = ?`, [member.user.id]).then(async res => {
-    if(await res.length == 0) {
-      database.query(`INSERT INTO ${member.guild.id}_guild_member_info (user_id, user_joined) VALUES (?, ?)`, [member.user.id, new Date()]).catch(err => {
-        return log.fatal(err);
-      });
-    }else {
-      if(res[0].user_joined == null) {
-        database.query(`UPDATE ${member.guild.id}_guild_member_info SET user_joined = ? WHERE user_id = ?`, [new Date(), member.user.id]).catch(err => {
-          return errorhandler(err, config.errormessages.databasequeryerror, null, log, config)
-        });
-      }
-      await database.query(`SELECT * FROM open_infractions WHERE user_id = ? AND guild_id = ? AND mute = ?`, [member.user.id, member.guild.id, 1]).then(async inf => {
-        if(await inf.length != 0) {
-          member.roles.add([member.guild.roles.cache.find(r => r.name === 'Muted')]);
-        }else {
-          let user_roles = await res[0].member_roles;
-          user_roles = JSON.parse(user_roles);
-
-          //? IF MUTED ROLE IS IN USERS DATASET -> MUTED ROLE WILL BE REMOVED
-          if(user_roles !== null && user_roles.indexOf(member.roles.cache.find(r => r.name === 'Muted')) !== -1) user_roles = user_roles.filter(val => {return val !== member.roles.cache.find(r => r.name === 'Muted').id});
-
-          giveAllRoles(member, member.guild, user_roles, bot);
-        }
-      }).catch(err => {
-        return log.fatal(err)
-      });
-    }
-    database.close();
-    return;
-  }).catch(err => {
-    return log.fatal(err)
-  });
-
-  database.query(`SELECT welcome_channel FROM ${member.guild.id}_config`).then(res => {
-    if (res[0].welcome_channel !== null) {
-      bot.channels.cache.find(c => c.id === res[0].welcome_channel).send('Welcome ' + member.user.username)
-    }
-  }).catch(err => { 
-    if(config.debug == 'true') console.log(err) 
-    return log.fatal(err);
-  })
-
-  database.query(`SELECT * FROM ${member.guild.id}_guild_joinroles`).then(res => {
-    for (i in res) {
-      let role = member.guild.roles.cache.find(r => r.id === res[i].role_id);
-      //setTimeout(function () {
-      try {
-        member.roles.add(role);
-      } catch (err) {
-        //NO PERMISSONS
-        return database.close();
-      }
-      // }, 10000);
-    }
-
-  }).catch(err => {
-    if(config.debug == 'true') console.log(err)
-    return log.fatal(err);
-  });
+bot.on('guildMemberAdd', async member => {
+  return await guildMemberAdd(member, bot)
 });
 
 
-bot.on('guildMemberRemove', member => {
-  const database = new Database();
-  
-  database.query(`SELECT * FROM ${member.guild.id}_guild_member_info WHERE user_id = ?`, [member.user.id]).then(async res => {
-    if(await res.length == 0) {
-      database.query(`INSERT INTO ${member.guild.id}_guild_member_info (user_id, member_roles) VALUES (?, ?)`, [member.user.id, JSON.stringify(await getAllRoles(member)) ]).catch(err => {
-        if(config.debug == 'true') console.log(err)
-        return log.fatal(err);
-      });
-    }else {
-      if(JSON.parse(res[0].member_roles) === await getAllRoles(member)) return;
-      else {
-        database.query(`UPDATE ${member.guild.id}_guild_member_info SET member_roles = ? WHERE user_id = ?`, [JSON.stringify(await getAllRoles(member)), member.user.id]).catch(err => {
-          if(config.debug == 'true') console.log(err)
-          return log.fatal(err);
-        })
-      }
-    }
-  }).catch(err =>{
-    if(config.debug == 'true') console.log(err);
-    return log.fatal(err)
-  });
+bot.on('guildMemberRemove', async member => {
+  return await guildMemberRemove(member);
 });
 
 
 
-//Command Manager
 bot.on("messageCreate", async message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-  if (message.author.system) return;
-  // blacklist(1, message);
-  // autoresponse(message);
-
-  const database = new Database();
-
-  await checkForScam(message, database, bot, config, log)
-  
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  var prefix;
-  database.query(`SELECT prefix FROM ${message.guild.id}_config`).then(async res => {
-    prefix = await res;
-
-    if (cmd.startsWith(prefix[0].prefix)) {
-
-      let commandfile = bot.commands.get(cmd.slice(prefix[0].prefix.length));
-      if (commandfile) { //&& blacklist(0, message)
-        database.query(`SELECT cooldown FROM ${message.guild.id}_config`).then(res => {
-          if(settingsCooldown.has(message.author.id) && cmd === `${prefix[0].prefix}settings`) {
-            database.close()
-            return message.channel.send(`You have to wait ${config.defaultSettingsCooldown.text} after each Settings Command.`);
-          }
-          else {
-            settingsCooldown.add(message.author.id);
-            setTimeout(async () => {
-              settingsCooldown.delete(message.author.id);
-            }, config.defaultSettingsCooldown.format);
-          }
-
-          if (defaultCooldown.has(message.author.id)) {
-            database.close()
-            return message.channel.send(`You have to wait ${res[0].cooldown / 1000 + 's'|| config.defaultCooldown.text} after each Command.`);
-          } else {
-            defaultCooldown.add(message.author.id);
-            commandfile.run(bot, message, args);
-
-            setTimeout(async () => {
-              defaultCooldown.delete(message.author.id);
-            }, res[0].cooldown || config.defaultCooldown.format);
-
-          }
-        }).catch(err => {
-          if(config.debug == 'true') console.log(err);
-          return log.fatal(err);
-        })
-      }
-    }else {
-        if(!levelCooldown.has(message.author.id)) {
-          gainXP(message, database);
-          levelCooldown.add(message.author.id);
-        }else {
-          setTimeout(() => {
-            levelCooldown.delete(message.author.id)
-          }, lvlconfig.timeout);
-        }
-    }
-  }).catch(err => {
-    if(config.debug == 'true') console.log(err)
-    return log.fatal(err);
-  });
+  return await messageCreate(message, bot);
 });
 
 process.on('unhandledRejection', err => {
@@ -225,16 +78,18 @@ process.on('uncaughtException', err => {
 })
 
 bot.once('ready', async () => {
-  checkInfractions(bot);
-  checkTemproles(bot)
+  checkInfractions(bot, new Database());
+  checkTemproles(bot, new Database())
   auditLog(bot);
+
   console.log(`****Ready! Logged in as  ${bot.user.tag}! I'm on ${bot.guilds.cache.size} Server****`);
+
   log.info('------------BOT SUCCESSFULLY STARTED------------', new Date());
+
   bot.user.setActivity({
-    name: "BETA",
-    type: 'PLAYING'
+    name: config.activity.playing.name,
+    type: config.activity.playing.type
   });
 });
-
 
 bot.login(token.BOT_TOKEN);
