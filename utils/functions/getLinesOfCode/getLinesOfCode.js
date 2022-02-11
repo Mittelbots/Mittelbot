64425984ce60cd86b090862504697ac60d8d68e6
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-11 17:01:49 +0100

    Update getLinesOfCode.js

--- utils/functions/getLinesOfCode/getLinesOfCode.js
+++ utils/functions/getLinesOfCode/getLinesOfCode.js
@@ -1,11 +1,9 @@
 const {exec} = require('child_process');
 
 async function getLinesOfCode(cb) {
-    var count;
-    var l = exec('git ls-files | xargs wc -l', (err, stdout, stderr) => {
+    exec('git ls-files | xargs wc -l', (err, stdout, stderr) => {
         if(err) {
             console.log(err);
-            return false;
         }
         let array = stdout.split(' ');
         let i = stdout.length;
