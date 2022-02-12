43100dd3e0dd63c258f34ef75db4d2725a946548
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-12 17:04:57 +0100

    bug fix

--- src/commands/moderation/infractions.js
+++ src/commands/moderation/infractions.js
@@ -13,24 +13,23 @@ module.exports.run = async (bot, message, args) => {
     }
 
     if (!await hasPermission(message, 0, 0)) {
-         
         message.delete();
         return message.channel.send(`<@${message.author.id}> ${config.errormessages.nopermission}`).then(msg => {
             setTimeout(() => msg.delete(), 5000);
         });
     }
 
-    var member;
+    var memberId;
 
     if(args[0] == undefined) {
-        member = await message.guild.members.fetch(message.author);
+        memberId = await message.guild.members.fetch(message.author).user.id;
     }else {
         try {
             args[0] = removeMention(args[0]);
-            member = await message.guild.members.fetch(args[0]) || args[0];
+            memberId = await message.guild.members.fetch(args[0]).user.id;
         }catch(err) {
-            member = args[0];
-            if(isNaN(member)) {
+            memberId = args[0];
+            if(isNaN(memberId)) {
                 return message.reply(`This is not a valid input!`).then(msg => setTimeout(() => msg.delete(), 5000));
             }
         }
@@ -38,12 +37,13 @@ module.exports.run = async (bot, message, args) => {
 
     var closed = [];
     var open = [];
-    await database.query(`SELECT * FROM closed_infractions WHERE user_id = ? ORDER BY ID DESC`, [member.user.id]).then(async res => closed.push(await res)).catch(err => {
+
+    await database.query(`SELECT * FROM closed_infractions WHERE user_id = ? ORDER BY ID DESC`, [memberId]).then(async res => closed.push(await res)).catch(err => {
         log.fatal(err);
         if(config.debug == 'true') console.log(err);
         return message.channel.send(`${config.errormessages.databasequeryerror}`); 
     });
-    await database.query(`SELECT * FROM open_infractions WHERE user_id = ? ORDER BY ID DESC`, [member.user.id]).then(async res => open.push(await res)).catch(err => {
+    await database.query(`SELECT * FROM open_infractions WHERE user_id = ? ORDER BY ID DESC`, [memberId]).then(async res => open.push(await res)).catch(err => {
         log.fatal(err);
         if(config.debug == 'true') console.log(err);
         return message.channel.send(`${config.errormessages.databasequeryerror}`); 
@@ -53,8 +53,8 @@ module.exports.run = async (bot, message, args) => {
         return message.reply(`This user dont have any infractions!`);
     }
     if(config.debug == 'true') console.info('Infraction Command passed!')
-    
-    return publicInfractionResponse(message, member, closed[0], open[0]);
+   
+    return await publicInfractionResponse(message, memberId, closed[0], open[0]);
 }
 
 module.exports.help = {
