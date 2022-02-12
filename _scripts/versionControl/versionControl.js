c41683a60663d393ea830dd1cf21a6ce79ebd293
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-12 20:32:14 +0100

    Update versionControl.js

--- _scripts/versionControl/versionControl.js
+++ _scripts/versionControl/versionControl.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 fs.readFile('./VERSION', 'utf-8', (err, data) => {
     if(err) throw err;
 
+    console.log(JSON.stringify(data))
     data = parseFloat(data);
     console.log(data);
 })
