57a02df45900bf8bd82241e21f1f90ec13a186b9
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-22 14:00:03 +0100

    checkForScam Bug hotfix

--- utils/checkForScam/checkForScam.js
+++ utils/checkForScam/checkForScam.js
@@ -12,6 +12,10 @@ async function checkForScam(message, bot, config, log) {
     axios.get('https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/domain-list.json').then(async res => {
         let data = res.data.domains;
         
+        if(message.content.search('discord.gg') !== -1 && message.content.indexOf('discord.gg') !== -1){
+            return;
+        }
+
         for(let i in data) {
             if(message.content.search(data[i]) !== -1 && message.content.indexOf(data[i]) !== -1) {
                 await banUser(await message.guild.members.fetch(message.author), message, `User tried to sent a Scam Link : ${data[i]}`, bot, config, log, getModTime('99999d'), 'Permanent', true)
