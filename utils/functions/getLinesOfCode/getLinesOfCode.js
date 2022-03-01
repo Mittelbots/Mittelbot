fcb382373c0a3e38d9a724d8e5df1c290cf9fe0e
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 21:53:59 +0100

    bug fix

--- utils/functions/getLinesOfCode/getLinesOfCode.js
+++ utils/functions/getLinesOfCode/getLinesOfCode.js
@@ -2,19 +2,24 @@ const {exec} = require('child_process');
 
 async function getLinesOfCode(cb) {
     exec('git ls-files | xargs wc -l', (err, stdout, stderr) => {
-        if(err.killed) {
-            console.log(err);
-        }
-        let array = stdout.split(' ');
-        let i = stdout.length;
-        while(i > 0) {
-            if(!isNaN(array[i])) {
-                return cb(array[i]);
-            }else {
-                i--;
+        try {
+            if(err) {
+                if(err.killed) {
+                    console.log(err);
+                }
             }
+            let array = stdout.split(' ');
+            let i = stdout.length;
+            while(i > 0) {
+                if(!isNaN(array[i])) {
+                    return cb(array[i]);
+                }else {
+                    i--;
+                }
+            }
+        }catch(err) {
+            console.log(err)
         }
-        
     });
 }
 module.exports = {getLinesOfCode};
