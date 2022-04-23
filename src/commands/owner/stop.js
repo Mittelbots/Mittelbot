88e8200da21399975a7704e76deb641f0316f396
Mittelblut9 <sauer.benedikt@outlook.de>
2022-04-23 23:16:56 +0200

    fixed owner commands

--- src/commands/owner/stop.js
+++ src/commands/owner/stop.js
@@ -5,11 +5,10 @@ const {execSync} = require('child_process');
 
 module.exports.run = async (bot, message, args, database) => {
     if(message.author.id === config.Bot_Owner_ID) {
-        return message.reply('Command disabled!').catch(err => {});
         try {
             message.reply(`Ok sir, Bot stopped!`).catch(err => {});
             log.info('------------BOT SUCCESSFULLY STOPPED------------');
-            execSync('npm run kill')
+            process.exit(1);
         }catch(err) {
             log.fatal(err);
             return message.reply(config.errormessages.general).catch(err => {});
