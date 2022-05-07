61f02910817f1e4c4dd1b5c15055a8af515bf4da
Mittelblut9 <sauer.benedikt@outlook.de>
2022-05-08 00:23:30 +0200

    added git

--- src/db/db_backup.js
+++ src/db/db_backup.js
@@ -4,4 +4,5 @@ const exec = require('child_process').exec;
 
 module.exports.db_backup = () => {
   exec(` mysqldump -u root -p${dbconfig.password} ${dbconfig.database} > ${dbconfig.backup_repo}/${new Date().getDay()+'_'+new Date().getMonth()+'_'+new Date().getFullYear()}.backup.sql`);
+  exec(` cd ${dbconfig.backup_repo} && git add . && git commit -m "${dbconfig.backup_repo}" && git push`);
 }
