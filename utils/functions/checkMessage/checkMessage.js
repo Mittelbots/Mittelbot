34f21c49845360fa24ec3863234bb1ab14554a5c
Mittelblut9 <sauer.benedikt@outlook.de>
2022-05-15 11:53:47 +0200

    fix

--- utils/functions/checkMessage/checkMessage.js
+++ utils/functions/checkMessage/checkMessage.js
@@ -4,7 +4,7 @@ async function checkMessage({author, guild, target, bot, type}) {
     if (target.id === author.id) return `You can't ${type} yourself.`;
     if (target.id === bot.user.id) return `You cant't ${type} me.`;
     if (target.bot || target.system) return `You can't ${type} a bot!`;
-    if (await isMod({member: guild.members.cache.get(author.id), guild})) return `You can't ${type} a mod!`;
+    if (await isMod({member: guild.members.cache.get(target.id), guild})) return `You can't ${type} a mod!`;
 }
 
 module.exports = {checkMessage}
