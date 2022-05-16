2b3f8fce4b55047b340ff611f5c55e97bc9c999f
Mittelblut9 <sauer.benedikt@outlook.de>
2022-05-16 22:58:36 +0200

    modlog fix

--- utils/functions/getEmote.js
+++ utils/functions/getEmote.js
@@ -1,6 +1,6 @@
 const config = require('../../src/assets/json/_config/config.json');
-function getEmote(bot, id) {
-    return bot.guilds.cache.get(config.DEVELOPER_DISCORD_GUILD_ID).emojis.cache.get(id);
+async function getEmote(bot, id) {
+    return await bot.guilds.cache.get(config.DEVELOPER_DISCORD_GUILD_ID).emojis.cache.get(id);
 }
 
 module.exports = {getEmote}
