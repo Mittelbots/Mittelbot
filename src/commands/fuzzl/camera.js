a7a19c7dee059e723b65a31e7582e008099d5149
Mittelblut9 <sauer.benedikt@outlook.de>
2022-07-11 19:55:06 +0200

    Added: Setting overview

--- src/commands/fuzzl/camera.js
+++ src/commands/fuzzl/camera.js
@@ -14,8 +14,22 @@ module.exports.run = async (bot, message, args) => {
         .setURL(video_link)
         .setImage(info.videoDetails.thumbnails[3].url)
 
+    const camera_setting = `**The best camera setting for F1 2022**\n
+Camera: **TV Pod Offset**
+Field of view: **1**
+Offset Lateral: **-12**
+Offset Horizontal: **20**
+Offset Vertical: **-5**
+Angle: **5**
+Near Clip Plane: **1**
+Mirror Angle: **-1**
+Camera Shake: **4**
+Camera Movement: **4**
+Lock to Apex Limit: **4**
+Halo Column: **On**`
+
     return message.reply({
-        content: video_link,
+        content: camera_setting,
         embeds: [lastUploadEmbed]
     }).catch(err => {})
 }
