a8aa01618388d37cb32c0745942573ce86067797
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-01 16:40:16 +0200

    Trying to fix it

--- dashboard/assets/js/settings/welcome.js
+++ dashboard/assets/js/settings/welcome.js
@@ -41,6 +41,7 @@ window.addEventListener('load', function () {
                     }
                     request.open('POST', `/api/change/${module}/${settings}/${type}/${guild_id}`);
                     request.setRequestHeader('Content-Type', 'application/json');
+                    request.setRequestHeader('Referrer-Policy', 'same-origin');
                     request.send();
 
                     await setTimeout(() => {
