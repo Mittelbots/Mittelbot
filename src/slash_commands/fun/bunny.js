c2a47ad8e97915548ea13776fd4e0bc7b8733fbf
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-07 13:33:53 +0200

    Improved code

--- src/slash_commands/fun/bunny.js
+++ src/slash_commands/fun/bunny.js
@@ -3,19 +3,23 @@ const {
     AttachmentBuilder
 } = require("discord.js");
 const animals = require('random-animals-api');
+const { errorhandler } = require("../../../utils/functions/errorhandler/errorhandler");
 
 module.exports.run = async ({
     main_interaction,
     bot
 }) => {
+    await main_interaction.deferReply();
+
     animals.bunny()
         .then(url => {
-            return main_interaction.reply({
+            return main_interaction.followUp({
                 files: [new AttachmentBuilder(url, 'bunny.png')]
             }).catch(err => {});
         })
         .catch((error) => {
-            main_interaction.reply({
+            errorhandler({err, fatal: true})
+            main_interaction.followUp({
                 content:'Something went wrong!',
                 ephemeral: true
             }).catch(err => {});
--- src/slash_commands/fun/dogs.js
+++ src/slash_commands/fun/dogs.js
@@ -1,16 +1,23 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
-
-const axios = require('axios');
+const animals = require('random-animals-api');
+const { errorhandler } = require("../../../utils/functions/errorhandler/errorhandler");
 
 module.exports.run = async ({main_interaction, bot}) => {
-    const { data } = await axios.get(`https://dog.ceo/api/breeds/image/random`);
-    if(data.status === 'success'){
-        return main_interaction.reply({
-            files: [new AttachmentBuilder(data.message, 'dog.png')]
-        }).catch(err => {});
-    }else {
-        return main_interaction.reply('Something went wrong!').catch(err => {});
-    }
+    await main_interaction.deferReply();
+
+    animals.dog()
+        .then(url => {
+            return main_interaction.followUp({
+                files: [new AttachmentBuilder(url, 'dog.png')]
+            }).catch(err => {});
+        })
+        .catch((error) => {
+            errorhandler({err, fatal: true})
+            main_interaction.followUp({
+                content:'Something went wrong!',
+                ephemeral: true
+            }).catch(err => {});
+        });
 }
 
 module.exports.data = new SlashCommandBuilder()
