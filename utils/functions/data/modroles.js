b9c8312cbaa897ee07e8e079e36f881599c898e0
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-09 13:19:10 +0200

    Changed database from modroles

---
+++ utils/functions/data/modroles.js
@@ -0,0 +1,50 @@
+const {
+    getGuildConfig, updateGuildConfig
+} = require("./getConfig");
+
+module.exports.updatePermsFromModroles = async ({guild_id, role_id, isadmin, ismod, ishelper}) => {
+    return new Promise(async (resolve, reject) => {
+        const config = await getGuildConfig({
+            guild_id
+        });
+        var modroles;
+        try {
+            modroles = JSON.parse(config.settings.modroles);
+        }catch(e) {
+            modroles = config.settings.modroles;
+        }
+
+        try {
+            modroles = JSON.parse(modroles);
+        } catch (e) {}
+
+        if (!modroles) modroles = [];
+
+        const obj = {
+            role: role_id,
+            isadmin: isadmin,
+            ismod: ismod,
+            ishelper: ishelper
+        }
+
+        const index = modroles.findIndex(x => x.role === role_id);
+
+        if (index !== -1) {
+            modroles[index] = obj;
+        }
+
+
+        modroles.push(obj);
+
+        return await updateGuildConfig({
+            guild_id,
+            value: JSON.stringify(modroles),
+            valueName: 'modroles'
+        }).then(async res => {
+            resolve(`✅ ${role_id} has been updated to ${isadmin ? 'Admin' : ismod ? 'Moderator' : 'Helper'}.`);
+        }).catch(err => {
+            reject(`❌ There was an error updating the reaction roles.`);
+        })
+    });
+
+}
