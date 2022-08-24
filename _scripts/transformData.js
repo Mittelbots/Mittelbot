928f67b2faac52238f4b898b336985bb0e1c71a5
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-24 14:52:52 +0200

    Added modrole db

--- _scripts/transformData.js
+++ _scripts/transformData.js
@@ -1,8 +1,8 @@
 const database = require("../src/db/db");
 const { getAllGuildIds } = require("../utils/functions/data/getAllGuildIds");
 
-const old_DB_name = "_guild_member_info";
-const new_DB_name = "member_info";
+const old_DB_name = "_guild_modroles";
+const new_DB_name = "guild_config";
 const isGuildIdRequired = true;
 
 async function main() {
@@ -28,6 +28,22 @@ async function main() {
                     .then(() => {console.log(`Successfully inserted new data. Status: ${d} of ${data.length}`);})
                     .catch(err => {console.error("Error occured", err);});
             }
+        }
+        if(new_DB_name == "guild_config") {
+            let mod_roles = [];
+            for(let d in data) {
+                const obj = {
+                    role: data[d].role_id,
+                    isadmin: data[d].isadmin,
+                    ismod: data[d].ismod,
+                    ishelper: data[d].ishelper
+                }
+                mod_roles.push(obj);
+            }
+
+            await database.query(`UPDATE ${new_DB_name} SET modroles = ? WHERE guild_id = ? `, [JSON.stringify(mod_roles), all_guilds[i].guild_id])
+                    .then(() => {console.log(`Successfully inserted new data.`);})
+                    .catch(err => {console.error("Error occured", err);});
         }
     }
 }
