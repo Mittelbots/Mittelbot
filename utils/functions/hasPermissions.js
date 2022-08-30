b813a2b6a4348dbf819167fa0488ab0c46edfa20
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-30 10:55:07 +0200

    Fixed: no permission bug

--- utils/functions/hasPermissions.js
+++ utils/functions/hasPermissions.js
@@ -1,110 +1,109 @@
-const { PermissionFlagsBits } = require("discord.js");
-const database = require("../../src/db/db");
 const {
-    getFromCache
-} = require("./cache/cache");
-const { errorhandler } = require("./errorhandler/errorhandler");
+    PermissionFlagsBits
+} = require("discord.js");
+const {
+    getGuildConfig
+} = require("./data/getConfig");
+const {
+    errorhandler
+} = require("./errorhandler/errorhandler");
+const config = require('../../src/assets/json/_config/config.json');
 
 
-async function hasPermission({
+module.exports.hasPermission = async({
     guild_id,
     adminOnly,
     modOnly,
     user,
-    isDashboard,
+    isDashboard = false,
     bot
-}) {
-
+}) => {
     let guildUser = bot.guilds.cache.get(guild_id).members.cache.get(user.id || user);
     let hasAdminPerms = guildUser.permissions.has(PermissionFlagsBits.Administrator);
-    
-    if(hasAdminPerms) return true;
 
-    const cache = await getFromCache({
-        cacheName: 'modroles',
-        param_id: guild_id
+    if (hasAdminPerms && !config.debug) return true;
+
+
+    const {
+        settings
+    } = await getGuildConfig({
+        guild_id
     });
+
+    const modroles = JSON.parse(settings.modroles) || settings.modroles || [];
 
     var role_id;
     var isadmin;
     var ismod;
     var ishelper;
-    if (!cache) {
-        return await database.query(`SELECT * FROM ${guild_id}_guild_modroles`).then(async (res) => {
-            var hasPermission = false
-            for (let i in await res) {
-                role_id = res[i].role_id;
-                isadmin = res[i].isadmin;
-                ismod = res[i].ismod;
-                ishelper = res[i].ishelper;
 
-                hasPermission = checkPerms({
-                    role_id,
-                    isadmin,
-                    ismod,
-                    ishelper
-                });
-                if (hasPermission) break;
-            }
-            return hasPermission;
-        }).catch(err => {
-            errorhandler({err, fatal: true});
-        })
+    var hasPermission = false
+    for (let i in modroles) {
+        role_id = modroles[i].role;
+        isadmin = modroles[i].isadmin;
+        ismod = modroles[i].ismod;
+        ishelper = modroles[i].ishelper;
 
-    } else {
-        for (let i in cache[0].modroles) {
-            role_id = cache[0].modroles[i].role_id;
-            isadmin = cache[0].modroles[i].isadmin;
-            ismod = cache[0].modroles[i].ismod;
-            ishelper = cache[0].modroles[i].ishelper;
+        hasPermission = this.checkPerms({
+            role_id,
+            isadmin,
+            ismod,
+            ishelper,
+            user: guildUser,
+            isDashboard,
+            modOnly,
+            adminOnly
+        });
 
-            hasPermission = checkPerms({
-                role_id,
-                isadmin,
-                ismod,
-                ishelper
-            });
-            if (hasPermission) break;
-        }
-        return hasPermission;
+        if (hasPermission) break;
     }
 
-    function checkPerms({
-        role_id,
-        isadmin,
-        ismod,
-        ishelper
-    }) {
-        var userHasRole;
-
-        try {
-            if (isDashboard) {
-                userHasRole = bot.guilds.cache.get(guild_id).members.cache.get(user).roles.cache.find(r => r.id === role_id);
-            } else {
-                userHasRole = user.roles.cache.find(r => r.id === role_id);
-            }
-        } catch (e) {
-            errorhandler({err, fatal: false, message: `${user.id} has tried a command with no permission in ${guild_id}`});
-            return false;
-        }
-
-        if (userHasRole) {
-            if (adminOnly && userHasRole && (ismod == 1 || ishelper == 1)) return false;
-            if (modOnly && userHasRole && ishelper == 1) {
-                errorhandler({fatal: false, message: `${user.id} has tried a command with no permission in ${guild_id}`});
-                return false
-            };
-            if (!isadmin && !ismod && !ishelper) return false;
-            if (userHasRole) {
-                return true;
-            }
-        } else {
-            errorhandler({fatal: false, message: `${user.id} has tried a command with no permission in ${guild_id}`});
-            return false;
-        }
+    if(!hasPermission) {
+        errorhandler({
+            fatal: false,
+            message: `${guildUser.id} has tried a command with no permission in ${guild_id}`
+        });
     }
+
+    return hasPermission;
 }
 
-module.exports = {
-    hasPermission
+module.exports.checkPerms = ({
+    role_id,
+    isadmin,
+    ismod,
+    ishelper,
+    user,
+    isDashboard,
+    adminOnly,
+    modOnly
+}) => {
+    var userHasRole;
+
+    if(user.roles.cache.size === 1) {
+        return false
+    };
+
+    try {
+        if (isDashboard) {
+            userHasRole = bot.guilds.cache.get(guild_id).members.cache.get(user).roles.cache.find(r => r.id === role_id);
+        } else {
+            userHasRole = user.roles.cache.find(r => r.id === role_id);
+        }
+    } catch (e) {
+        return false;
+    }
+
+    if (userHasRole) {
+        if (adminOnly && userHasRole && (ismod == 1 || ishelper == 1)) return false;
+        if (modOnly && userHasRole && ishelper == 1) {
+            return false
+        };
+        if (!isadmin && !ismod && !ishelper) return false;
+        if (userHasRole) {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
