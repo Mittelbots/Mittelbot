57da5beda50ae14d99a3fc496833b7681c622ca8
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-30 17:55:38 +0200

    Removed: old database query

--- utils/functions/isMod.js
+++ utils/functions/isMod.js
@@ -1,23 +1,31 @@
-const database = require("../../src/db/db");
-const { errorhandler } = require("./errorhandler/errorhandler");
+const {
+    getGuildConfig
+} = require("./data/getConfig");
 
+async function isMod({
+    member,
+    guild
+}) {
+    if (!member) return false;
 
-async function isMod({member, guild}) {
-    if(!member) return false;
-    
-    return await database.query(`SELECT * FROM ${guild.id}_guild_modroles`).then(async (res) => {
-        var isMod = false
-        for (let i in await res) {
-            if(member.roles.cache.find(r => r.id === res[i].role_id) !== undefined) {
-                isMod = true;
-                break;
-            }
-        }
-        return isMod;
-    }).catch(err => {
-        errorhandler({err});
-        return true;
+    const {
+        settings
+    } = getGuildConfig({
+        guild_id: guild.id
     })
+
+    const modroles = JSON.parse(settings.modroles) || settings.modroles || [];
+
+    var isMod = false
+    for (let i in await modroles) {
+        if (member.roles.cache.find(r => r.id === modroles[i].role) !== undefined) {
+            isMod = true;
+            break;
+        }
+    }
+    return isMod;
 }
 
-module.exports = {isMod}
+module.exports = {
+    isMod
+}
