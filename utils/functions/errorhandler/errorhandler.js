07d8feb4520afbbf9a0011fd8c2ec85c64534c1c
Mittelblut9 <sauer.benedikt@outlook.de>
2022-09-07 20:51:11 +0200

    Fix: errorhandler crashed the bot & new log desgin

--- utils/functions/errorhandler/errorhandler.js
+++ utils/functions/errorhandler/errorhandler.js
@@ -4,19 +4,18 @@ const callerId = require('caller-id');
 
 module.exports.errorhandler = ({err = 'No error passed! ', message = 'No message passed! ', channel = null, fatal = true}) => {
     const caller = callerId.getData();
-
     let errObj = {
-        'Error:': err.toString(),
         'Message': message,
         'Called From': caller.filePath,
-        'Line': caller.lineNumber
+        'Line': caller.lineNumber,
+        '------------': '------------'
     }
 
-    if(config.debug) console.log(errObj);
+    if(config.debug) log.fatal(err, '\n', JSON.stringify(errObj, null, 4));
 
-    else if(fatal && log) log.fatal(errObj);
+    else if(fatal && log) log.fatal(err, '\n', JSON.stringify(errObj, null, 4));
 
-    else if(!fatal) debug_log.info(errObj);
+    else if(!fatal) debug_log.info(err, '\n', JSON.stringify(errObj, null, 4));
 
     if(channel) return channel.send(message).catch(err => {});
     else return;
