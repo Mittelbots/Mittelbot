ea6a89cf59e1ced0d1db743e701bb4b4f5ed6063
Mittelblut9 <sauer.benedikt@outlook.de>
2022-09-23 18:04:29 +0200

    Changed function to promise

--- utils/functions/moderations/kickUser.js
+++ utils/functions/moderations/kickUser.js
@@ -7,39 +7,37 @@ const { getCurrentFullDate } = require("../data/dates");
 const { insertIntoClosedList } = require("../data/infractions");
 const { errorhandler } = require("../errorhandler/errorhandler");
 
-async function kickUser({user, mod, guild, reason, bot}) {
-    let pass = false;
+module.exports.kickUser = ({user, mod, guild, reason, bot}) => {
+    return new Promise(async (resolve, reject) => {
+        let pass = false;
 
-    privateModResponse(user, config.defaultModTypes.kick, reason, null, bot, guild.name);
-    await guild.members.kick(user, {
-        reason: reason
-    })
-    .then(() => pass = true)
-
-    if(pass) {
-        insertIntoClosedList({
-            uid: user.id,
-            modid: mod.id,
-            ban: 0,
-            mute: 0,
-            warn: 0,
-            kick: 0,
-            reason,
-            infraction_id: await createInfractionId(),
-            start_date: getCurrentFullDate(),
-            guild_id: guild.id
+        privateModResponse(user, config.defaultModTypes.kick, reason, null, bot, guild.name);
+        await guild.members.kick(user, {
+            reason: reason
         })
-        setNewModLogMessage(bot, config.defaultModTypes.kick, mod.id, user, reason, null, guild.id);
-        const p_response = await publicModResponses(config.defaultModTypes.kick, mod, user.id, reason, null, bot);
-        errorhandler({fatal: false, message: `${mod.id} has triggered the kick command in ${guild.id}`});
-        
-        return p_response;
-    }else {
-        return {
-            error: true,
-            message: config.errormessages.nopermissions.kick
-        }
-    }
-}
+        .then(() => pass = true)
+        .catch(err => pass = false)
 
-module.exports = {kickUser}
+        if(pass) {
+            insertIntoClosedList({
+                uid: user.id,
+                modid: mod.id,
+                ban: 0,
+                mute: 0,
+                warn: 0,
+                kick: 0,
+                reason,
+                infraction_id: await createInfractionId(),
+                start_date: getCurrentFullDate(),
+                guild_id: guild.id
+            })
+            setNewModLogMessage(bot, config.defaultModTypes.kick, mod.id, user, reason, null, guild.id);
+            const p_response = await publicModResponses(config.defaultModTypes.kick, mod, user.id, reason, null, bot);
+            errorhandler({fatal: false, message: `${mod.id} has triggered the kick command in ${guild.id}`});
+            
+            return resolve(p_response);
+        }else {
+            return reject(config.errormessages.nopermissions.kick);
+        }
+    })
+}
