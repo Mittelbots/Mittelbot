b24034a9cc31955e119c533737327f5d02bbc31d
Mittelblut9 <sauer.benedikt@outlook.de>
2022-11-02 20:59:19 +0100

    Rewrote database query

--- utils/functions/data/all_guild_id.js
+++ utils/functions/data/all_guild_id.js
@@ -1,16 +1,16 @@
-const database = require('../../../src/db/db');
+const allGuildId = require('../../../src/db/Models/tables/allGuildId');
 const { errorhandler } = require('../errorhandler/errorhandler');
 
 module.exports.insertIntoAllGuildId = async (guild_id) => {
-    return new Promise(async (resolve, reject) => {
-        await database
-            .query(`INSERT IGNORE INTO all_guild_id (guild_id) VALUES (?)`, [guild_id])
-            .then(() => {
-                return resolve(true);
-            })
-            .catch((err) => {
-                errorhandler({ err });
-                return reject(false);
-            });
+    allGuildId.create(
+        {
+            guild_id: guild_id,
+        }
+    ).then(() => {
+        resolve(true);
+    })
+    .catch(err => {
+        errorhandler({ err });
+        return reject(false);
     });
 };
