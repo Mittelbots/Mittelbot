1313a866799cb98dab4f614d94e3bf375771a4a7
Mittelblut9 <info@blackdayz.de>
2022-12-25 00:59:48 +0100

    add/basic structure

--- src/slash_commands/help/tutorial.js
+++ src/slash_commands/help/tutorial.js
@@ -1,32 +1,62 @@
+const { StringSelectMenuBuilder } = require('discord.js');
+const { ActionRowBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports.run = async ({ main_interaction, bot }) => {
-    await main_interaction.deferReply({
-        ephemeral: true,
-    });
-
     const mainEmbed = new EmbedBuilder()
         .setTitle('Tutorial')
         .setDescription(
             `Learn everything important about the Bot.\nPlease select one of options in the Dropdown menu to get started.\nYou can add more questions and answers in the official discord support server https://mittelbot.blackdayz.de/support`
         )
-        .setImage(`https://i.pinimg.com/originals/76/ba/79/76ba795f3cd29fa90b05e52381cbf929.gif`);
+        .setImage(`https://media.giphy.com/media/VXCPgZwEP7f1e/giphy-downsized-large.gif`);
 
-    const dropdownMenu = new SelectMenuBuilder()
-        .setCustomId('tutorial')
-        .setPlaceholder('Choose an option');
-
-    menu.addOptions([
-        {
-            value: `message_${main_interaction.guild.id}`,
-            label: 'Message over of the embed',
-        },
-    ]);
+        const row = new ActionRowBuilder()
+            .addComponents(new StringSelectMenuBuilder()
+            .setCustomId('tutorial')
+            .setPlaceholder('Choose a topic to learn more about.')
+            .addOptions([
+            {
+                value: `commands`,
+                description: 'Wonder how to use a command?',
+                label: 'Slash Commands',
+            },
+            {
+                value: `permissions`,
+                description: 'Learn how the permission system works.',
+                label: 'Permissions',
+            },
+            {
+                value: `notifications`,
+                description: 'Learn how to get notifications from the Bot.',
+                label: 'Notifications (yt, twitch, ...)',
+            },
+            {
+                value: `level`,
+                description: 'Learn how the level system works.',
+                label: 'Level System',
+            },
+            {
+                value: `moderation`,
+                description: 'Learn how the moderation works.',
+                label: 'Moderation',
+            },
+            {
+                value: `automod`,
+                description: 'Learn how the automoderation works.',
+                label: 'Automoderation',
+            },
+            {
+                value: `scam`,
+                description: 'Learn how the scam protection works.',
+                label: 'Scam Protection',
+            }
+        ]));
 
     await main_interaction
-        .followUp({
+        .reply({
             embeds: [mainEmbed],
+            components: [row],
         })
         .catch((err) => {});
 };
