0870278d9ea915f8bb3d8b7d5f2fd9e1ca08861b
Mittelblut9 <info@blackdayz.de>
2022-12-28 12:24:19 +0100

    fix/error on older messages

--- bot/messageDelete.js
+++ bot/messageDelete.js
@@ -1,6 +1,7 @@
 const Auditlog = require('../utils/functions/data/Auditlog');
 
 module.exports.messageDelete = async (bot, message) => {
+    if(!message.author) return;
     const auditLog = new Auditlog();
     await auditLog.init(bot, message.guild.id);
     await auditLog.messageDelete(message);
