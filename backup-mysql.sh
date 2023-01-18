fe7b8ae6e2faa9520d9a5a2ed68aa0cc2a79480c
Mittelblut9 <info@blackdayz.de>
2023-01-18 20:23:13 +0100

    wip/database backup

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -8,38 +8,33 @@
 #----------------------------------------
 USER=$DB_USER # MySQL User
 PASSWORD=$DB_PASSWORD # MySQL Password
-DAYS_TO_KEEP=5    # 0 to keep forever
-GZIP=0            # 1 = Compress
-BACKUP_PATH='./backups/mysql'
+DB_NAME=$DB_DATABASE # Database Name
+HOST=$DB_HOST # MySQL Hostname
+
 #----------------------------------------
 
-# Create the backup folder
-if [ ! -d $BACKUP_PATH ]; then
-  mkdir -p $BACKUP_PATH
-fi
+echo "Dumping database $DB_NAME to backup.sql"
 
-# Get list of database names
-databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
+while true
+do
+  YEAR=$(date +"%Y")
+  MONTH=$(date +"%m")
+  DAY=$(date +"%d")
+  BACKUP_FILE="$YEAR-$MONTH-$DAY.sql"
 
-for db in $databases; do
+  echo "Creating backup file $BACKUP_FILE"
 
-  if [ $db == 'information_schema' ] || [ $db == 'performance_schema' ] || [ $db == 'mysql' ] || [ $db == 'sys' ]; then
-    echo "Skipping database: $db"
-    continue
+  mkdir -p ./backups/$YEAR/$MONTH/$DAY
+
+  mysqldump -u $USER -p $PASSWORD $DB_NAME > ./backups/$BACKUP_FILE
+  cat ./backups/$BACKUP_FILE
+  if [ ! -s ./backups/$BACKUP_FILE ]
+  then
+    echo "Backup file is empty, deleting"
+    rm ./backups/$BACKUP_FILE
+  else 
+    echo "-------Backup complete-------"
   fi
-  
-  date=$(date -I)
-  if [ "$GZIP" -eq 0 ] ; then
-    echo "Backing up database: $db without compression"      
-    mysqldump -u $USER -p$PASSWORD --databases $db > $BACKUP_PATH/$date-$db.sql
-  else
-    echo "Backing up database: $db with compression"
-    mysqldump -u $USER -p$PASSWORD --databases $db | gzip -c > $BACKUP_PATH/$date-$db.gz
-  fi
+    
+  sleep 86400
 done
-
-# Delete old backups
-if [ "$DAYS_TO_KEEP" -gt 0 ] ; then
-  echo "Deleting backups older than $DAYS_TO_KEEP days"
-  find $BACKUP_PATH/* -mtime +$DAYS_TO_KEEP -exec rm {} \;
-fi
