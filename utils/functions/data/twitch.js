a373266a77d8b6bcf87912943e57d1f717493eb1
Mittelblut9 <info@blackdayz.de>
2023-01-27 23:01:16 +0100

    fix/twitch notifier

--- utils/functions/data/twitch.js
+++ utils/functions/data/twitch.js
@@ -3,89 +3,158 @@ const { twitchApiClient } = require('../../../src/events/notfifier/twitch_notifi
 const { errorhandler } = require('../errorhandler/errorhandler');
 const twitchStreams = require('../../../src/db/Models/tables/twitchStreams.model');
 
-module.exports.changeTwitchNotifier = async ({ twitchchannel, twdcchannel, twpingrole, guild }) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            var twitch_user = await twitchApiClient.users.getUserByName(twitchchannel);
-        } catch (err) {
-            return reject(`❌ I couldn't find the channel you have entered.`);
-        }
-        if (!twitch_user) {
-            return reject(`❌ I couldn't find the channel you have entered.`);
-        }
+module.exports = class TwitchNotifier {
+    constructor() {}
 
-        if (!guild.members.me) {
-            await guild.members.fetch(process.env.DISCORD_APPLICATION_ID);
-        }
-        try {
-            var hasChannelPerms = twdcchannel
-                .permissionsFor(guild.members.me)
-                .has([
-                    PermissionFlagsBits.ViewChannel,
-                    PermissionFlagsBits.SendMessages,
-                    PermissionFlagsBits.EmbedLinks,
-                    PermissionFlagsBits.AttachFiles,
-                    PermissionFlagsBits.MentionEveryone,
-                ]);
-        } catch (err) {
-            errorhandler({
-                err,
-                fatal: true,
+    change({ twitchchannel, twdcchannel, twpingrole, guild }) {
+        return new Promise(async (resolve, reject) => {
+            const twitch_user = await this.#getTwitchFromChannelName(twitchchannel);
+
+            const twitchId = twitch_user.id;
+            const twitchName = twitch_user.name;
+
+            if (!twitch_user) {
+                return reject(`❌ I couldn't find the channel you have entered.`);
+            }
+
+            await this.#fetchMembers({ guild });
+            const hasChannelPermission = await this.#checkChannelPerms({ twdcchannel, guild });
+            if (!hasChannelPermission) {
+                reject(
+                    `❌ I don't have one of these permissions \`"VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "MENTION_EVERYONE"\`. Change them and try again.`
+                );
+                return;
+            }
+
+            const isAvailable = await this.#checkAvailability({
+                twitchchannel: twitchId,
+                guild,
+            }).catch(() => {
+                reject(`❌ You can only add 3 twitch channels. Remove one and try again.`);
+                return '3';
             });
-            reject(`❌ Something went wrong while checking the permissions. Please try again.`);
-            return false;
-        }
 
-        if (!hasChannelPerms) {
-            reject(
-                `❌ I don't have one of these permissions \`"VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "MENTION_EVERYONE"\`. Change them and try again.`
-            );
-            return;
-        }
+            if (isAvailable === '3') return;
 
-        const allChannelsFromGuild = await twitchStreams
-            .findAll({
-                where: {
-                    guild_id: guild.id,
-                },
-            })
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
+            if (!isAvailable) {
+                await this.#updateTwitchChannel({
+                    twitchchannelId: twitchId,
+                    twdcchannel,
+                    twpingrole,
+                    guild,
+                }).then(() => {
+                    resolve(`✅ Successfully updated the twitch channel \`${twitchchannel}\`.`);
+                });
+            } else {
+                await this.#createTwitchChannel({
+                    twitchchannelId: twitchId,
+                    twitchchannelName: twitchName,
+                    twdcchannel,
+                    twpingrole,
+                    guild,
+                }).then(() => {
+                    resolve(
+                        `✅ Successfully added the twitch channel \`${twitchchannel}\` to the list.`
+                    );
+                });
+            }
+        });
+    }
+
+    #getTwitchFromChannelName(channelname) {
+        return new Promise(async (resolve) => {
+            try {
+                const twitch_user = await twitchApiClient.users.getUserByName(channelname);
+                resolve(twitch_user);
+            } catch (err) {
                 errorhandler({
                     err,
                     fatal: true,
                 });
-                reject(
-                    `❌ Something went wrong while selecting all youtube channels. Please contact the Bot support.`
+                resolve(false);
+            }
+        });
+    }
+
+    #fetchMembers({ guild }) {
+        return new Promise(async (resolve) => {
+            if (!guild.members.me) {
+                await guild.members.fetch(process.env.DISCORD_APPLICATION_ID);
+            }
+
+            resolve(true);
+        });
+    }
+
+    #checkChannelPerms({ twdcchannel, guild }) {
+        return new Promise(async (resolve) => {
+            try {
+                const hasChannelPerms = twdcchannel
+                    .permissionsFor(guild.members.me)
+                    .has([
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.EmbedLinks,
+                        PermissionFlagsBits.AttachFiles,
+                        PermissionFlagsBits.MentionEveryone,
+                    ]);
+                return resolve(hasChannelPerms);
+            } catch (err) {
+                errorhandler({ err });
+                resolve(false);
+            }
+        });
+    }
+
+    getAllChannelsFromGuild({ guild }) {
+        return new Promise(async (resolve, reject) => {
+            const allChannelsFromGuild = await twitchStreams
+                .findAll({
+                    where: {
+                        guild_id: guild.id,
+                    },
+                })
+                .then((res) => {
+                    return res;
+                })
+                .catch((err) => {
+                    errorhandler({
+                        err,
+                        fatal: true,
+                    });
+                    reject(
+                        `❌ Something went wrong while selecting all youtube channels. Please contact the Bot support.`
+                    );
+                    return false;
+                });
+
+            if (!allChannelsFromGuild) return resolve(false);
+
+            resolve(allChannelsFromGuild);
+        });
+    }
+
+    #checkAvailability({ twitchchannel, guild }) {
+        return new Promise(async (resolve, reject) => {
+            const allChannelsFromGuild = await this.getAllChannelsFromGuild({ guild });
+            if (allChannelsFromGuild.length > 0) {
+                const twChannelExists = allChannelsFromGuild.filter(
+                    (channel) => channel.channel_id == twitchchannel
                 );
-                return false;
-            });
 
-        if (!allChannelsFromGuild) return;
-
-        let twChannelExists;
-        if (allChannelsFromGuild.length > 0) {
-            twChannelExists = allChannelsFromGuild.filter(
-                (channel) => channel.channel_id === twitch_user.id
-            )[0];
-
-            if (allChannelsFromGuild.length >= 3 && !twChannelExists) {
-                return reject(`You already have 3 twitch channels. You have to delete one first.`);
+                if (allChannelsFromGuild.length >= 3 && !twChannelExists) {
+                    return reject('3');
+                }
+                if (twChannelExists.length > 0) {
+                    return resolve(false);
+                }
             }
-        }
+            resolve(true);
+        });
+    }
 
-        if (twChannelExists) {
-            if (
-                twitch_user.id === twChannelExists.channel_id &&
-                twdcchannel.id === twChannelExists.info_channel_id &&
-                twpingrole === twChannelExists.pingrole
-            ) {
-                reject(`❌ You are trying to add the same config you've already added.`);
-                return;
-            }
-
+    #updateTwitchChannel({ twitchchannelId, twdcchannel, twpingrole, guild }) {
+        return new Promise(async (resolve, reject) => {
             twitchStreams
                 .update(
                     {
@@ -95,47 +164,45 @@ module.exports.changeTwitchNotifier = async ({ twitchchannel, twdcchannel, twpin
                     {
                         where: {
                             guild_id: guild.id,
-                            channel_id: twitch_user.id,
+                            channel_id: twitchchannelId,
                         },
                     }
                 )
                 .then(() => {
-                    resolve(
-                        `✅ Successfully updated the twitch channel settings for ${twChannelExists.channel_name}.`
-                    );
+                    resolve(true);
                 })
                 .catch((err) => {
                     errorhandler({
                         err,
                         fatal: true,
                     });
-                    reject(
-                        '❌ Something went wrong while updating the data. Please contact the Bot support.'
-                    );
+                    reject(false);
                 });
-        } else {
+        });
+    }
+
+    #createTwitchChannel({ twitchchannelId, twitchchannelName, twdcchannel, twpingrole, guild }) {
+        return new Promise(async (resolve, reject) => {
             twitchStreams
                 .create({
                     guild_id: guild.id,
-                    channel_id: twitch_user.id,
+                    channel_id: twitchchannelId,
                     info_channel_id: twdcchannel.id,
                     pingrole: twpingrole ? twpingrole.id : null,
-                    channel_name: twitchchannel,
+                    channel_name: twitchchannelName,
                 })
                 .then(() => {
-                    resolve(`✅ Successfully added ${twitchchannel} to the notification list.`);
+                    resolve(true);
                 })
                 .catch((err) => {
                     errorhandler({
                         err,
                         fatal: true,
                     });
-                    reject(
-                        '❌ Something went wrong while adding the channel to the database. Please contact the Bot support.'
-                    );
+                    reject(false);
                 });
-        }
-    });
+        });
+    }
 };
 
 module.exports.delTwChannelFromList = async ({ guild_id, deltwchannel }) => {
