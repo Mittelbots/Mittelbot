ac818947832b59151cdb28527acc446695dbaa40
Mittelblut9 <info@blackdayz.de>
2023-02-02 23:04:38 +0100

    fix/public beta prepare

--- src/slash_commands/fun/avatar.js
+++ src/slash_commands/fun/avatar.js
@@ -1,17 +1,26 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { errorhandler } = require('../../../utils/functions/errorhandler/errorhandler');
 
 module.exports.run = async ({ main_interaction, bot }) => {
     const user = main_interaction.options.getUser('user') || main_interaction.user;
 
     const newEmbedBuilder = new EmbedBuilder()
         .setColor('#0099ff')
-        .setTitle(`**Click to view ${user.username} Avatar**`)
+        .setTitle(`**Click to view ${user.username} Avatar in the browser**`)
         .setURL(`${user.displayAvatarURL()}`)
         .setImage(user.displayAvatarURL({ format: 'png' }) + '?size=1024')
         .setTimestamp();
-    return main_interaction.reply({
-        embeds: [newEmbedBuilder],
-    });
+    return main_interaction
+        .reply({
+            embeds: [newEmbedBuilder],
+        })
+        .catch((err) => {
+            errorhandler({
+                error: err,
+                message: 'Missing Permissions',
+                isFatal: false,
+            });
+        });
 };
 
 module.exports.data = new SlashCommandBuilder()
