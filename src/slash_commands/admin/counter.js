e32afb43c768ebd93885d71b475423c3615b829c
Mittelblut9 <info@blackdayz.de>
2023-04-30 19:47:26 +0200

    add/translation at command

--- src/slash_commands/admin/counter.js
+++ src/slash_commands/admin/counter.js
@@ -1,14 +1,13 @@
+const { EmbedBuilder } = require('discord.js');
 const Counter = require('../../../utils/functions/data/Counter/Counter');
 const { counterConfig, counterPerms } = require('../_config/admin/counter');
 
 module.exports.run = async ({ main_interaction, bot }) => {
     await main_interaction.deferReply({ ephemeral: true });
 
-    const counterApi = new Counter();
-
-    const channel_id = main_interaction.options.getChannel('channel').id;
     const command = main_interaction.options.getSubcommand();
 
+    const counterApi = new Counter();
     const counter = await counterApi.get(main_interaction.guild.id).catch((err) => {
         return main_interaction.followUp({ content: err, ephemeral: true });
     });
@@ -16,7 +15,16 @@ module.exports.run = async ({ main_interaction, bot }) => {
     if (command === 'remove') {
         if (!counter) {
             return main_interaction.followUp({
-                content: 'Counter does not exist',
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            global.t.trans(
+                                ['error.admin.counter.notFound'],
+                                main_interaction.guild.id
+                            )
+                        )
+                        .setColor(global.t.trans(['general.colors.error'])),
+                ],
                 ephemeral: true,
             });
         }
@@ -24,24 +32,87 @@ module.exports.run = async ({ main_interaction, bot }) => {
         return await counterApi
             .delete(main_interaction.guild.id)
             .then(() => {
-                return main_interaction.followUp({ content: 'Counter removed', ephemeral: true });
+                return main_interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setDescription(
+                                global.t.trans(
+                                    ['success.fun.counter.removed'],
+                                    main_interaction.guild.id
+                                )
+                            )
+                            .setColor(global.t.trans(['general.colors.success'])),
+                    ],
+                    ephemeral: true,
+                });
             })
             .catch((err) => {
-                return main_interaction.followUp({ content: err, ephemeral: true });
+                return main_interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setDescription(
+                                global.t.trans(
+                                    ['error.generalWithMessage', err],
+                                    main_interaction.guild.id
+                                )
+                            )
+                            .setColor(global.t.trans(['general.colors.error'])),
+                    ],
+                    ephemeral: true,
+                });
             });
     }
 
+    const channel = main_interaction.options.getChannel('channel');
+    const channel_id = channel.id;
+
     if (counter) {
-        return main_interaction.followUp({ content: 'Counter already exists', ephemeral: true });
+        return main_interaction.followUp({
+            embeds: [
+                new EmbedBuilder()
+                    .setDescription(
+                        global.t.trans(
+                            ['error.admin.counter.alreadyExists'],
+                            main_interaction.guild.id
+                        )
+                    )
+                    .setColor(global.t.trans(['general.colors.error'])),
+            ],
+            ephemeral: true,
+        });
     }
 
     return await counterApi
         .create(guild_id, channel_id)
         .then(() => {
-            return main_interaction.followUp({ content: 'Counter created', ephemeral: true });
+            return main_interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            global.t.trans(
+                                ['success.fun.counter.set', channel],
+                                main_interaction.guild.id
+                            )
+                        )
+                        .setColor(global.t.trans(['general.colors.success'])),
+                ],
+                ephemeral: true,
+            });
         })
         .catch((err) => {
-            return main_interaction.followUp({ content: err, ephemeral: true });
+            return main_interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            global.t.trans(
+                                ['error.generalWithMessage', err],
+                                main_interaction.guild.id
+                            )
+                        )
+                        .setColor(global.t.trans(['general.colors.error'])),
+                ],
+                ephemeral: true,
+            });
         });
 };
 
