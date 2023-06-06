1349e9d2d6b1be00fa0e41148520a5da5c0897bd
Mittelblut9 <info@blackdayz.de>
2023-06-06 12:11:38 +0200

    fix/prettier bugs

--- bot/core/apiAuth.js
+++ bot/core/apiAuth.js
@@ -1,25 +1,26 @@
 const axios = require('axios');
 
 module.exports = () => {
-    console.log(process.env.BOT_AUTH_KEY);
-    if (process.env.BOT_AUTH_KEY === ' ') {
-        axios
-            .post('http://localhost:5000/auth', {
-                headers: {
-                    accept: 'application/json',
-                },
-            })
-            .then((res) => {
-                for (let i = 0; i < 6; i++) {
-                    if (i === 3) {
-                        console.info(res.data);
-                        continue;
-                    }
-                    console.info('COPY THIS TOKEN AND INSERT IT IN YOUR ENVIRONMENT VARIABLES');
-                }
-            })
-            .catch((err) => {
-                console.log(err.response.data);
-            });
-    }
-};
+    console.log(process.env.BOT_AUTH_KEY)
+            if(process.env.BOT_AUTH_KEY === ' ') {
+                axios
+                    .post('http://localhost:5000/auth', {
+                        headers: {
+                            accept: 'application/json',
+                        },
+                    })
+                    .then((res) => {
+                        for(let i = 0; i < 6; i++) {
+                            if(i === 3) {
+                                console.info(res.data);
+                                continue;
+                            }
+                            console.info('COPY THIS TOKEN AND INSERT IT IN YOUR ENVIRONMENT VARIABLES');
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err.response.data);
+                    });
+
+            }
+}
