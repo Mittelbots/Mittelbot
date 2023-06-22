502eb513da9e6b53c1a6e5bd4ac038bced52e169
Mittelblut9 <info@blackdayz.de>
2023-06-22 12:59:24 +0200

    style: prettier changes

--- tests/unit/src/db/db.test.js
+++ tests/unit/src/db/db.test.js
@@ -2,7 +2,8 @@ const database = require('~src/db/db');
 
 describe('Database is available', () => {
     it('should not throw any errors and load all models', async () => {
-        await database.init()
+        await database
+            .init()
             .then((result) => {
                 expect(result).toBe(true);
             })
