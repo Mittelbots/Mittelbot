7e8149605d53c5ce991cfd3be4589551337f2585
Mittelblut9 <info@blackdayz.de>
2023-08-26 01:49:15 +0200

    fix: update isValidHexCode check

--- utils/functions/validate/isValidHexCode.js
+++ utils/functions/validate/isValidHexCode.js
@@ -1,19 +1,20 @@
-module.exports.isValidHexCode = async (str) => {
-    if (str[0] != '#') return false;
+module.exports.isValidHexCode = (str) => {
+    if (str[0] !== '#') return false;
 
     if (!(str.length === 4 || str.length === 7)) return false;
 
-    for (let i = 1; i < str.length; i++)
+    for (let i = 1; i < str.length; i++) {
+        const charCode = str[i].charCodeAt(0);
         if (
             !(
-                (str[i].charCodeAt(0) <= '0'.charCodeAt(0) && str[i].charCodeAt(0) <= 9) ||
-                (str[i].charCodeAt(0) >= 'a'.charCodeAt(0) &&
-                    str[i].charCodeAt(0) <= 'f'.charCodeAt(0)) ||
-                str[i].charCodeAt(0) >= 'A'.charCodeAt(0) ||
-                str[i].charCodeAt(0) <= 'F'.charCodeAt(0)
+                (charCode >= '0'.charCodeAt(0) && charCode <= '9'.charCodeAt(0)) ||
+                (charCode >= 'a'.charCodeAt(0) && charCode <= 'f'.charCodeAt(0)) ||
+                (charCode >= 'A'.charCodeAt(0) && charCode <= 'F'.charCodeAt(0))
             )
-        )
+        ) {
             return false;
+        }
+    }
 
     return true;
 };
