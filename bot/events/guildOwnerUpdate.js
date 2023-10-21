4795767163238f6dd47f09ebab9afb685b3e845d
Mittelblut9 <info@blackdayz.de>
2023-10-21 13:28:58 +0200

    fix: wrong variable call when guild owner changes

--- bot/events/guildOwnerUpdate.js
+++ bot/events/guildOwnerUpdate.js
@@ -2,13 +2,13 @@ const Auditlog = require('~utils/classes/Auditlog');
 
 module.exports.guildOwnerUpdate = async (bot, oldGuild, newGuild) => {
     const auditLog = new Auditlog();
-    await auditLog.init(bot, newGuild.guild.id);
+    await auditLog.init(bot, newGuild.id);
     await auditLog.setEmbed({
         color: '#B22222',
         text: `**Owner has changed**\n**Old Owner**\n${oldGuild.owner}\n**New Owner**\n${newGuild.owner}`,
     });
     await auditLog.sendToAuditLog({
-        guildId: newGuild.guild.id,
-        target: newGuild.guild,
+        guildId: newGuild.id,
+        target: newGuild,
     });
 };
